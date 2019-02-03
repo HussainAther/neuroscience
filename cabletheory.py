6f9d2498817dd65f346a6410fd4f2bd1641d535e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-03 00:56:36 -0500

    init comment

--- cabletheory.py
+++ cabletheory.py
@@ -4,4 +4,6 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from gekko import GEKKO
 
-
+"""
+When modeling circuits using cable theory, we assume the segments are cylinders with constant radii.
+"""
