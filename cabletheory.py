114d88e80a2736f80116d76019b04952d8400bd0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-03 00:58:40 -0500

    more words lol

--- cabletheory.py
+++ cabletheory.py
@@ -6,4 +6,10 @@ from gekko import GEKKO
 
 """
 When modeling circuits using cable theory, we assume the segments are cylinders with constant radii.
+The electrotonic potential is due to a change in the membrane potential.
+At any instant of time, the resting membrane potential (E) can be changed
+by several means. We can inject current, use extracellular currents, and
+discover changes in membrane conductance caused by driving forces.
+
+As such, the current in the neuron behaves in accordance with mathematical theory.
 """
