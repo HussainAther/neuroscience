8b3ae121e01831d961c5723861f8a06b962ae9ea
Syed Hussain Ather <shussainather@gmail.com>
2019-02-03 00:42:00 -0500

    total conductance equation and info

---
+++ singlecurrent.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+"""
+The sign of a synaptic action can be predicted by knowledge of the relationship between resting potential (V) and
+reversal potential (E). WE can determine amplitude between synpatic conductance and the extra synaptic
+conductances. Creating equivalent electrical cirucits we can move from a single-channel conductacne to that of macroscopic
+conductances and currents. We can determine the total conductance change produced by simultaneous activvation of many
+synpases. Gamma is the single-channel conductance, P is the probabilty of opening a single channel, and N is the total
+number of ligand-gated channels in the post-synaptic conductacne
+"""
+# function that returns total conductance
+def total_conductance(gamma, P, N)
+    g_syn = gamma*P*N # Add conductances in parallel for simultaneous activation
+    return g_syn
