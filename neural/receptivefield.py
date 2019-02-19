660ff14c7f57ee72950dd416a726f03162717a70
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 17:21:15 -0500

    history

--- neural/receptivefield.py
+++ neural/receptivefield.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+"""
+In 1952 Hubel and Wiesel made an interesting discovery. When they inserted thin electrodes into cat's visual cortex and presented small spots of light on a screen in front of the cat, some of the cells responded by firing spikes only when the spot was at a particular positions.
+The same cell would be inhibited by spots located in different areas of visual field or it would not respond at all.
+The responses of such neurons were fully characterised by the inhibitory and excitatory areas in the visual field. They called such neurons simple cells.
+
+They also found another type of cells that manifested more complex responses. These cells, called complex cells, would respond only to a visual object of a particular shape and orientation no matter where it was positioned in the visual field. If the image was slightly rotated
+or its shape was changed, the response was supressed.
+"""
