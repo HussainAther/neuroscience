4a7fd9ee24d886b1fe23d144b2d579be63d6102a
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 17:28:25 -0500

    function

--- neural/receptivefield.py
+++ neural/receptivefield.py
@@ -10,7 +10,16 @@ The responses of such neurons were fully characterised by the inhibitory and exc
 They also found another type of cells that manifested more complex responses. These cells, called complex cells, would respond only to a visual object of a particular shape and orientation no matter where it was positioned in the visual field. If the image was slightly rotated
 or its shape was changed, the response was supressed.
 """
-
+def receptive_field(XX, YY, theta, width, height):
+    """
+    Generate a receptive field.
+    """
+    x_r = XX * np.cos(theta) + YY * np.sin(theta)
+    y_r = -XX * np.sin(theta) + YY * np.cos(theta)
+    gaussian = np.exp(-np.pi*(y_r**2/height**2)) * (np.abs(x_r) < width)
+    F = 1./width * 0.75
+    complex_grating = np.exp(1j*2*np.pi*F*x_r)
+    return gaussian * complex_grating.real
 
 """
 Receptive field is the set of the stimuli that evoke responses in single neurons. In case of visual receptive fields these are patches of light impaling on retina that are most effecitve in driving a neuron. It is customary to represent visual receptive field as images showing the preffered stimuli.
@@ -19,29 +28,25 @@ We will now generate a sample receptive field using numpy. We start with a perio
 only one of the coordinates. This is best illustrated by the following schematics:
 """
 
-xmin, xmax, dx = -20, 20, 0.04 #degree
+xmin, xmax, dx = -20, 20, 0.04
 ymin, ymax, dy = -20, 20, 0.04
 
 YY, XX = np.mgrid[xmin:xmax:dx, ymin:ymax:dy]
 
-"""
-Generate sine pattern in x-direction.
-"""
-
 spatial_freq = 0.1
 rf = np.sin(2 * np.pi * spatial_freq * XX)
-plt.matshow(rf)
-plt.gray()
 
+rf_height = 10
+rf_width = 4
+rf_angle = 0.
+rf_ampl = 1. #Hz
 
 rf_angle = np.pi/3.
 
 """
 Use rotated system of coordinates.
 """
-
-x_r = XX * np.cos(rf_angle) + YY * np.sin(rf_angle)
-y_r = -XX * np.sin(rf_angle) + YY * np.cos(rf_angle)
+W = rf_ampl * receptive_field(XX, YY, rf_angle, rf_width, rf_height)
 
 rf = np.cos(2 * np.pi * spatial_freq * x_r)
 plt.matshow(rf)
@@ -60,3 +65,18 @@ mask_y = np.exp(-(y_r**2/rf_height**2))
 rf *= mask_x * mask_y
 
 plt.matshow(rf)
+
+
+plt.figure(figsize=(6,2.5))
+plt.subplot(121)
+plt.imshow(W, extent=(xmin, xmax, xmin, xmax), aspect='equal')
+plt.xlabel('position (deg)')
+
+plt.axhline(0, ls='--', color='k')
+plt.gray()
+cbar = plt.colorbar()
+plt.subplot(122)
+plt.plot(XX[500,:], W[500, :])
+plt.xlabel('position (deg)')
+cbar.set_label('receptive field')
+plt.tight_layout()
