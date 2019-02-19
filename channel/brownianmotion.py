b33d2aa17965c1035659bca92a5a890225c81947
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 16:04:15 -0500

    channel

---
+++ channel/brownianmotion.py
@@ -0,0 +1,30 @@
+import math
+import numpy
+import random
+import decimal
+import scipy.linalg
+import numpy.random as nrand
+import matplotlib.pyplot as plt
+
+"""
+It's like walking when you're drunk. Don't try this at home. 
+"""
+
+def brownian_motion_log_returns(param):
+    """
+    This method returns a Wiener process. The Wiener process is also called Brownian motion. For more information
+    about the Wiener process check out the Wikipedia page: http://en.wikipedia.org/wiki/Wiener_process
+    :param param: the model parameters object
+    :return: brownian motion log returns
+    """
+    sqrt_delta_sigma = math.sqrt(param.all_delta) * param.all_sigma
+    return nrand.normal(loc=0, scale=sqrt_delta_sigma, size=param.all_time)
+
+
+def brownian_motion_levels(param):
+    """
+    Returns a price sequence whose returns evolve according to a brownian motion
+    :param param: model parameters object
+    :return: returns a price sequence which follows a brownian motion
+    """
+    return convert_to_prices(param, brownian_motion_log_returns(param))
