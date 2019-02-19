ab31ab40043e75c333d897477929160af569a818
Syed Hussain Ather <shussainather@gmail.com>
2019-02-18 19:22:24 -0500

    totalProb

--- informationtheory.py
+++ informationtheory.py
@@ -2,6 +2,15 @@
 
 """
 When we observe spike train of a sensory neuron, we learn about many different aspects of the stimulus.
-This way, we gain information. Entropy is closely related to information. We can think about information transmission by sensory neurons in terms of
-various parts of hte spike train as part of stimulus space.
+This way, we gain information. Shannon entropy is closely related to information. We can think about information transmission by sensory neurons in terms of
+various parts of the spike train as part of stimulus space.
+
+
 """
+
+def totalProb(variableProbs):
+    """
+    The additivity of information mean that for any distribution of N independent variables,
+    the probability of each of them occuring is the product of each of the individual variables.
+    """
+    return product(variableProbs)
