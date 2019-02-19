f080ab17890d28564b2f4c8567049d8f7e3a5d48
Syed Hussain Ather <shussainather@gmail.com>
2019-02-18 19:16:42 -0500

    more comment woo

--- informationtheory.py
+++ informationtheory.py
@@ -2,5 +2,6 @@
 
 """
 When we observe spike train of a sensory neuron, we learn about many different aspects of the stimulus.
-This way, we gain information.
+This way, we gain information. Entropy is closely related to information. We can think about information transmission by sensory neurons in terms of
+various parts of hte spike train as part of stimulus space.
 """
