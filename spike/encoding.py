189762ab51f2f117c4bfdc0b94e8d55c101eaf98
Syed Hussain Ather <shussainather@gmail.com>
2019-02-20 12:25:44 -0500

    math

--- spike/encoding.py
+++ spike/encoding.py
@@ -13,3 +13,25 @@ The second property says that adding two light intensities together should produ
 is equal to the sum of the firing rates to each individual lights. This assumption of linearity does
 not hold really well in reality as neural firing rates cannot increase indefinitely.
 """
+
+# Definition of the stimulus
+dt = 0.01                # Time step [s]
+T  = 2                   # Total duration of the simulation
+t = np.arange(0, 2, dt)  # Time points
+
+f = 0.5 # Frequency
+A = 1   # Amplitude
+stim = A*np.sin(2*np.pi*f*t) + np.random.normal(0.0, 0.05, len(t))
+
+# Delayed amplification
+response = np.zeros(len(stim))
+delay = 50
+amplification = 1.5
+
+for i in range(delay,len(stim)):
+    response[i] = amplification*stim[i-delay]
+
+plt.plot(t,stim)
+plt.plot(t,response)
+plt.xlabel('Time (s)')
+plt.legend(['Stimulus','response'])
