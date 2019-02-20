7a7860504b5c392d0ef07346f284ad67b9c2e64e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-20 12:41:03 -0500

    visual cortex

--- spike/encoding.py
+++ spike/encoding.py
@@ -82,3 +82,26 @@ plt.plot(rgc[20,:])
 plt.axis('off')
 plt.axhline(0,color='k')
 plt.title('Receptive field cross section')
+
+# Primal visual cortex single cells
+[X,Y] = np.meshgrid(np.arange(-2,2,0.1),np.arange(-2,2,0.1))
+
+f     = 0.6     # Spatial frequency
+theta = 0.0     # Orientation
+phi   = np.pi/2 # Phase offset
+sigma = 0.3     # Standard deviation of the gaussian kernel
+gamma = 0.6     # Aspect ratio
+
+X_rot = X*np.cos(theta) + Y*np.sin(theta)
+Y_rot = -X*np.sin(theta) + Y*np.cos(theta)
+
+g = np.exp( - (X_rot**2 + (gamma**2)*(Y_rot**2))/(2*sigma**2))*np.cos(2*np.pi*f*X_rot + phi)
+plt.subplot(1,2,1)
+plt.title('Simple cell RF')
+plt.axis('off')
+plt.imshow(g)
+plt.subplot(1,2,2)
+plt.plot(g[20,:])
+plt.title('RF cross-section')
+plt.axis('off')
+plt.axhline(0,color='k')
