6031ea6806ea81e8b45523df161d2364cbc01f46
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 13:32:15 -0500

    initial commit

---
+++ spike/integrateandfire.py
@@ -0,0 +1,6 @@
+
+"""
+In the leaky integrate-and-fire model of the neuron, we add a "leak" to the membrane potential
+that reflects the diffusion of ions that occurs throguh the membrane when some equilibrium is not
+reached in the cell. we model:
+"""
