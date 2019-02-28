7d4b9efb1f2a4846c8fa2b9917b45262eab42992
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 14:19:47 -0500

    comment import

--- synaptic/hopfield.py
+++ synaptic/hopfield.py
@@ -1,4 +1,10 @@
+import numpy as np
 
 """
-John Hopfield generalized 
+John Hopfield combined a generalized form of the Hebb rule with simple activation
+dynamics to construct an energy function in which the stored memory states
+were local minima of the energy function. It allowed Hebbian learning to lead
+to attrcator memories and strengthened the bridge between memory networks
+and certain branches of statistical mechanics.
 """
+
