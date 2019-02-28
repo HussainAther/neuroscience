2b6fb96d07a1ba8e8cc2b942150ce13971bde2bd
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 13:11:47 -0500

    comment

--- spike/ratefunction.py
+++ spike/ratefunction.py
@@ -7,6 +7,9 @@ to mark each spike. Use the Dirac delta function centered at t = t_hat:
 1. delta(t) = 0 for t != 0
 2. integral from a to b of delta(t)dt = 1 for a <= 0 <= b.
 3. integral from a to b of f(t)*delta(t) = f(0) for any function f(t) and for a <= 0 <= b.
+
+We solve this using a convolution integral of two functions. These smoothing functions can convert
+messy, noisy data into spike trains that can be analyzed.
 """
 
 A = [1,2,3,4,5,6,7,8,9]
