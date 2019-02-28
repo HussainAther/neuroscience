52f22c48591a6f11fdda2845a9e2cdf2e43c9c42
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 13:07:11 -0500

    convolve

--- spike/ratefunction.py
+++ spike/ratefunction.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 """
 Represent spike train  as a function of time to place an infinitely thin, infinitely tall window
@@ -6,6 +7,9 @@ to mark each spike. Use the Dirac delta function centered at t = t_hat:
 1. delta(t) = 0 for t != 0
 2. integral from a to b of delta(t)dt = 1 for a <= 0 <= b.
 3. integral from a to b of f(t)*delta(t) = f(0) for any function f(t) and for a <= 0 <= b.
-
-
 """
+
+A = [1,2,3,4,5,6,7,8,9]
+b = [1,2,3]
+
+np.convolve(A, b)
