77d78be67d356730d44f2940df124c38f922c665
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 16:18:21 -0500

    Comment

--- neural/kinematics.py
+++ neural/kinematics.py
@@ -2,5 +2,7 @@
 """
 In neuroscience, we use kinetmatic  transformations to determine sensory-motor questions
 of how the nervous system can choose a single configuartion of body parts (such as arms and legs) in space.
-The coordinate transformations themselves
+We can use mathematical methods of transformation and processing input values themselves to model
+kinematics trnasformations. We can study how the central nervous system if the predictable
+patterns of variability in limb movement can be attributed to these mathematical coordinate transformations.
 """
