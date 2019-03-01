8b3169bc14c8fa188fa3e3deadc62659cd3801af
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 17:04:47 -0500

    import

---
+++ neural/neuralnetworks/feedforward.py
@@ -0,0 +1,11 @@
+import time
+import random
+random.seed(int(time.time())) # some random seed lol
+
+"""
+A feedforward neural network is an artificial neural network wherein
+connections between the nodes do not form a cycle. As such, it is
+different from recurrent neural networks. The feedforward neural
+ network was the first and simplest type of artificial neural network devised.
+"""
+
