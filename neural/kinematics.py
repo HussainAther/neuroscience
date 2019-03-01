add5f3e2a392c60edd4ab122780fc14b31bf21dc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 16:42:45 -0500

    minjerk

--- neural/kinematics.py
+++ neural/kinematics.py
@@ -11,6 +11,27 @@ patterns of variability in limb movement can be attributed to these mathematical
 We define the states of a neuron using ordinary differential equations.
 """
 
+def minjerk(H1x,H1y,H2x,H2y,t,n):
+    """
+    Given hand initial position H1x,H1y, final position H2x,H2y and movement duration t,
+    and the total number of desired sampled points n,
+    Calculates the hand path H over time T that satisfies minimum-jerk.
+    
+    Flash, Tamar, and Neville Hogan. "The coordination of arm
+        movements: an experimentally confirmed mathematical model." The
+        journal of Neuroscience 5, no. 7 (1985): 1688-1703.
+    
+    """
+    T = linspace(0,t,n)
+    Hx = zeros(n)
+    Hy = zeros(n)
+    for i in range(n):
+        tau = T[i]/t
+        Hx[i] = H1x + ((H1x-H2x)*(15*(tau**4) - (6*tau**5) - (10*tau**3)))
+        Hy[i] = H1y + ((H1y-H2y)*(15*(tau**4) - (6*tau**5) - (10*tau**3)))
+    return T,Hx,Hy
+
+
 # Set up some parameters that we can work with.
 E_params = {
     "E_leak" : -7.0e-2,
@@ -73,7 +94,8 @@ params = {
 
 def neuron(state, t, params):
     """
-    Following the properties of Ekeberg, et al., 1991, we can create a model of a neuron
+    Following the properties of Ekeberg, et al., 1991, ("A computer based
+    model for realistic simulations of neural networks") we can create a model of a neuron
     to simulate neural networks using the soma and the dendritic tree.
     """
 
