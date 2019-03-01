eec6f266b39fc2465752b286982f192626eb1dc9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 17:10:05 -0500

    random

--- neural/neuralnetworks/feedforward.py
+++ neural/neuralnetworks/feedforward.py
@@ -1,6 +1,7 @@
 import time
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 random.seed(int(time.time())) # some random seed lol
 
 """
@@ -28,16 +29,16 @@ xor_out = matrix([[0.0],
 
 # initialize weights and biases to small random values
 sigw = 0.5
-w_hid = rand(2,2)*sigw        # [inp1,inp2] x-> [hid1,hid2]
-b_hid = rand(1,2)*sigw        # 1.0 -> [b_hid1,b_hid2]
-w_out = rand(2,1)*sigw        # [hid1,hid2] x-> [out1]
-b_out = rand(1,1)*sigw        # 1.0 -> [b_out1]
-w_out_prev_change = zeros(shape(w_out))
-b_out_prev_change = zeros(shape(b_out))
-w_hid_prev_change = zeros(shape(w_hid))
-b_hid_prev_change = zeros(shape(b_hid))
+w_hid = random.rand(2,2)*sigw        # [inp1,inp2] x-> [hid1,hid2]
+b_hid = random.rand(1,2)*sigw        # 1.0 -> [b_hid1,b_hid2]
+w_out = random.rand(2,1)*sigw        # [hid1,hid2] x-> [out1]
+b_out = random.mrand(1,1)*sigw        # 1.0 -> [b_out1]
+w_out_prev_change = np.zeros(shape(w_out))
+b_out_prev_change = np.zeros(shape(b_out))
+w_hid_prev_change = np.zeros(shape(w_hid))
+b_hid_prev_change = np.zeros(shape(b_hid))
 
 maxepochs = 1000
-errors = zeros((maxepochs,1))
+errors = np.zeros((maxepochs,1))
 N = 0.01 # learning rate parameter
 M = 0.10 # momentum parameter
