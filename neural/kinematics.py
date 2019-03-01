61f69e2840d210fec06b36c5a26b5363e5b97dcb
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 16:11:36 -0500

    initial commit

---
+++ neural/kinematics.py
@@ -0,0 +1,6 @@
+
+"""
+In neuroscience, we use kinetmatic  transformations to determine sensory-motor questions
+of how the nervous system can choose a single configuartion of body parts (such as arms and legs) in space.
+The coordinate transformations themselves
+"""
