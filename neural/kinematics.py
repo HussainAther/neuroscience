0a3b66aba3e0bcdf674b1cc43bfc15cc60dddc24
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 16:28:12 -0500

    neuron

--- neural/kinematics.py
+++ neural/kinematics.py
@@ -1,3 +1,4 @@
+from scipy.integrate import odeint
 
 """
 In neuroscience, we use kinetmatic  transformations to determine sensory-motor questions
@@ -5,4 +6,20 @@ of how the nervous system can choose a single configuartion of body parts (such 
 We can use mathematical methods of transformation and processing input values themselves to model
 kinematics trnasformations. We can study how the central nervous system if the predictable
 patterns of variability in limb movement can be attributed to these mathematical coordinate transformations.
+
+We define the states of a neuron using ordinary differential equations.
 """
+
+def neuron(state, t, params):
+    """
+    Following the properties of Ekeberg, et al., 1991, we can create a model of a neuron
+    to simulate neural networks using the soma and the dendritic tree.
+    """
+
+    E = state[0]    # soma potential
+    m = state[1]    # Na activation
+    h = state[2]    # Na inactivation
+    n = state[3]    # K activation
+    q = state[4]    # Ca activation
+    CaAP = state[5] # Ca2+ dependent K channel
+
