2509f4bc299698d6a0cca63402b11923616b5dfb
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 14:54:22 -0500

    popprob

--- spike/decoding.py
+++ spike/decoding.py
@@ -3,12 +3,28 @@ import numpy as np
 Estimate static stimulus values on the basis of spike-counting firing rates.
 We estimate such a stimulus from the sequence of firing times of the spikes that the stimulus evokes.
 
+If r is the spike-count firing rate of neuron a, we can obtain an estimate of the wind direction
+on any given trial from the direction of the vector v_pop.
+
+v_pop = the sum of (r/r_max)*c_a from a=1 to 4.
+
+If we assume each neuron in a popultion fires independently, the firing-rate probability
+for the population is the product of the individual probabilities:
 
 """
 
+def popProb():
+    """
+    population probability.
+    """
+    result = 1
+    for prob in problist:
+        factor = exp(-prob*T)
 
+        result *= factor
 
 """
+
 The Cramér-Rao bound sets a limit of the variance of any estimate s_est according to:
 
 (sigma_est)^2 >= (1 + b'_est(s))^2 / I_F(s)
