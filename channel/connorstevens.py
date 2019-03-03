49805d467931c14dede83ecc248c728cc29b5417
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 17:37:40 -0500

    Def

--- channel/connorstevens.py
+++ channel/connorstevens.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 """
 The Hodgkin-Huxley model was developed on the basis of data from the giant squid axon.
@@ -6,3 +7,13 @@ fast Na+, delayed rectifier K+, and leakaage conductsances (as the HH model does
 conductances have properties somewhat different. The C-S model als ohas an extra K+ conductance
 (know nas the A-current) that is transient.
 """
+
+dt = 0.000005
+tmax = 2
+
+def I_m():
+    """
+    Membrane current for Connor-Stevens Model.
+    """
+    g_L =.003
+    g_Na =
