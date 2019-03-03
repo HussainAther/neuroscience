a8d8c5935d0cf3419e4f7f0a46383333f98ef578
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 17:50:10 -0500

    Itot

--- channel/connorstevens.py
+++ channel/connorstevens.py
@@ -16,7 +16,7 @@ vclamp_flag = 0 # otherwise this should be 1, for voltage clamp conditions
 
 istart = 0.5 # time applied current starts
 ilength = 1 # length of applied current pulse
-Ie=2.405e-7 # magnitude of applied current pulse
+Ie = 2.405e-7 # magnitude of applied current pulse
 Ie = 0.78e-7 # threshold for constant spiking with no A-current
 
 vstart = 0.25 # time to step voltage
@@ -51,22 +51,20 @@ b = np.zeros(len(t)) # A-current inactivation gating variable
 
 Iapp = np.zeros(len(t)) # Applied current, relevant in current-clamp mode
 if iclamp_flag: # i.e. if in current-clamp mode
-    for i in range(round(istart/dt), round((istart+ilength)/dt+1)) # make non-zero for duration of current pulse
+    for i in range(round(istart/dt), round((istart+ilength)/dt)+1) # make non-zero for duration of current pulse
         Iapp[i] = Ie
 
 Vapp = np.zeros(len(t)) # Applied voltage, relevant in voltage-clamp mode
-if ( vclamp_flag )
-    for i = 1:round(vstart/dt)          % % make V0 before pulse
-        Vapp(i) = V0;
-    end
-    for i=round(vstart/dt)+1:round((vstart+vlength)/dt) % make Ve for duration of voltage pulse
-        Vapp(i) = Ve;
-    end
-    for i=round((vstart+vlength)/dt):length(Vapp) % make V0 following pulse
-        Vapp(i) = V0;
-    end
-end
+if vclamp_flag:
+    for i in range(round(vstart/dt)) # make V0 before pulse
+        Vapp[i] = V0
+    for i in range(round(vstart/dt), round((vstart+vlength)/dt)+1) # make Ve for duration of voltage pulse
+        Vapp(i) = Ve
+    for i in range(round((vstart+vlength)/dt), length(Vapp)+1) # make V0 following pulse
+        Vapp(i) = V0
 
+# total current
+Itot = np.zeros(len(t)) # in case we want to plot and look at the total current
 
 def I_m():
     """
