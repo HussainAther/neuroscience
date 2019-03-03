16316cb4c224236a329e4701b12a7c3f3ac61a59
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 17:35:37 -0500

    initial commit

---
+++ channel/connorstevens.py
@@ -0,0 +1,7 @@
+
+"""
+The Hodgkin-Huxley model was developed on the basis of data from the giant squid axon.
+The Connor-STevens model provides an alternate descirption of action-potential general using
+fast Na+, delayed rectifier K+, and leakaage conductsances (as the HH model does), but the Na+ and K+
+conductances have properties somewhat different.
+"""
