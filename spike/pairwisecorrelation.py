29f90a2f3b33644a948414c09a65be6a41b834e3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 18:33:34 -0500

    cov

--- spike/pairwisecorrelation.py
+++ spike/pairwisecorrelation.py
@@ -17,7 +17,7 @@ of neuron A increases over its mean value if measured within a fixed window of s
 
 def corrCoef(i, j):
     """
-    Calculate the NxN matrix of pairwise Pearson's correlation coefficients between all combinations of N binned
+    Calculate the len(i)x(len(j) matrix of pairwise Pearson's correlation coefficients between all combinations of N binned
     spike trains. For each pair of spike trains (equal-sized arrays i and j), we determine the correlation by binning i and j
     at the desired bin size. Let b_i and b_j denote the binary vectors and m_i and m_j their respective averages.
     """
@@ -27,10 +27,20 @@ def corrCoef(i, j):
     b_j = np.where(j > 0.5, 1, 0) # binary vector of i
     result = np.zeros((1,len(i),len(i)))
     for a in range(len(i)):
-        for b in range(len(i)):
+        for b in range(len(j)):
             num = np.multiply(b_i[a]-m_i[a], b_j[b]-m_j[b])
             den = np.sqrt(np.multiply(b_i[a]-m_i[a], b_i[b]-m_i[b]) * np.multiply(b_j[a]-m_j[a], b_j[b]-m_j[b]))
             result[a][b] = num / den
     return result
 
+def covariance(i, j):
+    """
+    Calculate the len(i)x(len(j) matrix of pairwise covariances between all combinations of N binned spike trains.
+    See the comment for the corrCoef function.
+    """
+    m_i = sum(i)/len(i) # average of a
+    m_j = sum(j)/len(j) # average of b
+    b_i = np.where(i > 0.5, 1, 0) # binary vector of i
+    b_j = np.where(j > 0.5, 1, 0) # binary vector of i
+    result = np.zeros((1,len(i),len(i)))
 
