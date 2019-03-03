529bcb6e37aeb2999735987e80bc7a7253b18c45
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 18:09:41 -0500

    initial commit

---
+++ spike/pairwisecorrelation.py
@@ -0,0 +1,12 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+"""
+Quantifying the degree of correlation between neural spike trains is a key part of analyses of experimental data in many systems.
+Neural coordination is thought to play a key role in information propagation and processing and also in self-organization of the
+neural system during development. For example, correlated activity plays a critical role in forming the retinotopic map.
+In the developing retina, waves of correlated spontaneous activity in retinal ganglion cells have been recorded [on multielectrode arrays (MEAs) and by calcium imaging]
+in vitro in many species (Wong, 1999), and shown in vivo using calcium imaging in mouse (Ackman et al., 2012). These waves show both temporal and spatial correlations.
+Much work has focused on assessing the role of this activity in the development of the retinotopic map; typically, both the map and various statistics of the activity are compared between wild-type and mutant genotypes. The results are used to make inferences about which features of the activity are implicated in retinotopic map formation (Stafford et al., 2009). There is strong evidence that correlation between neuronal spike times is involved in this process (Xu et al., 2011).
+
+"""
