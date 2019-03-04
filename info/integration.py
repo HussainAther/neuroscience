af27ac4d949a2d91e90aab9c99a937414561396e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 20:28:34 -0500

    cue comment

--- info/integration.py
+++ info/integration.py
@@ -7,5 +7,9 @@ visual and auditory stimuli at the same tiem and same location in time and space
 
 Following "Bayesian inference with probabilistic population codes" (2006) by Ma et al.,
 we can express the mathematical distribution of subject's estimates of these stimuli.
+
+In a cue combination task, the goal is to integrate two cues, c1 and c2, both of which provide
+information about the same stimulus, s. For instance, s could be the spatial location of a stimulus,
+c1 could be a visual cue for the location, and c2 could be an auditory cue. 
 """
 
