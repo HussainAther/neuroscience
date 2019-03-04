ed22f0d3fd6b2c3ad36d2364f44921d4cdaa5914
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 20:31:46 -0500

    mu and sigma

--- info/integration.py
+++ info/integration.py
@@ -10,6 +10,17 @@ we can express the mathematical distribution of subject's estimates of these sti
 
 In a cue combination task, the goal is to integrate two cues, c1 and c2, both of which provide
 information about the same stimulus, s. For instance, s could be the spatial location of a stimulus,
-c1 could be a visual cue for the location, and c2 could be an auditory cue. 
+c1 could be a visual cue for the location, and c2 could be an auditory cue.
+
+The mean and variance of posterior mu and sigma combine the auditory and visual inputs.
+
+mu3 = (sigma2^2 / (sigma1^2 + sigma2^2)) * mu1 + (sigma1^2/(sigma1^2+sigma2^2) * mu2
+
+1/(sigma3^2) = 1/sigma1^2 + 1/sigma2^2
+
 """
 
+def visionDist():
+    """
+    
+    """
