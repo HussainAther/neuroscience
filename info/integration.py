90c83343648e2fa96f56e10323f2f7dc73bad7e2
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 20:22:38 -0500

    comment; import

--- info/integration.py
+++ info/integration.py
@@ -1,5 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Humans use Bayesian optimal integration in the information from both
 visual and auditory stimuli at the same tiem and same location in time and space.
+
+Following "Bayesian inference with probabilistic population codes" (2006) by Ma et al.,
+we can express the mathematical distribution of subject's estimates of these stimuli.
 """
+
