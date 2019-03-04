4c88c9c30fee41fef51cd63c9d541ff18c17082c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 19:46:17 -0500

    plot

--- synaptic/alphasynapse.py
+++ synaptic/alphasynapse.py
@@ -31,8 +31,8 @@ def alpha_():
     t_peak = 1 # ms
     g_peak = 0.05 # nS (peak synaptic conductance)
     const = g_peak / (t_peak*np.exp(-1));
-    t_vec = np.arange(0, t_a + h, h)
-    alpha_func = const * t_vec * (np.exp(-t_vec/t_peak))
+    t_vec = np.arange(0, t_a + h, h) # time used for plotting
+    alpha_func = const * t_vec * (np.exp(-t_vec/t_peak)) # calculate alpha function 
 
     plt.plot(t_vec[:80], alpha_func[:80])
     plt.xlabel("t (in ms)")
@@ -44,5 +44,20 @@ C = 0.5 # capacitance nF
 R = 40 # leak resistance M ohms
 
 g_ad = 0 # conductance
-G_inc = 1/h # spike rate adaptation 
+G_inc = 1/h # spike rate adaptation
 tau_ad = 2
+
+E_leak = -60 # mV, equilibrium potential
+E_syn = 0 # Excitatory synapse (why is this excitatory?)
+g_syn = 0 # Current syn conductance
+V_th = -40 # spike threshold mV
+V_spike = 50 # spike value mV
+ref_max = 4/h # Starting value of ref period counter
+t_list = np.array([], dtype=int)
+V = E_leak
+V_trace = [V]
+t_trace = [0]
+
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(np.arange(0,t_max,h), spike_train)
+axs[0].set_title("Input spike train")
