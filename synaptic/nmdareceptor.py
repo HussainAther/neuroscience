df1cac2c3ee750b8f790472c0f35bb9b2ac7db33
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 20:00:08 -0500

    initial commit

---
+++ synaptic/nmdareceptor.py
@@ -0,0 +1,6 @@
+
+"""
+The NMDA receptor conductance effects the postsynaptic potnetial not normally
+seen in other conductances. The NMDA receptor current uses a factor that
+depends on postsynpatic potential V. We write it as g_NMDA * G_NMDA * P(V-E_NMDA) in which
+"""
