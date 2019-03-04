5b19779b6665ee2a457b7d074225a59960ed0a6c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-03 19:24:04 -0500

    initial commit

---
+++ channel/frequencyresponse.py
@@ -0,0 +1,10 @@
+
+
+
+"""
+e
+A passive neural membrane is a shift-invariant linear system. According to Eq. 8, the membrane
+potential fluctuations (above and below E) may be predicted by convolving the injected current
+with a low-pass filter. The parameters of that low-pass filter, g and C, completely determine the
+behavior of the membrane.
+"""
