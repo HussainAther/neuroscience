29379ebaefa649bb39f8e38478e487482001f188
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 20:02:45 -0500

    initial commit

---
+++ channel/ohmicconductance.py
@@ -0,0 +1,14 @@
+
+"""
+In states of nonequilibrium the Nernst potential (V_n) doesn't need to equal the actual
+potential jump across a membrane. The flux of ions through a membrane will be dissipative and
+proportional to a net driving force.
+
+Ohmic conductance hypothesis dictates:
+j_qi = z_ie*j_i = (deltaV - V_n) * g_i
+
+in which j_i is the number of ions of type i per area per time crossing the membrane,
+j_qi is the electric charge flux, z_ie is the charge on one ion, and g_i is the constant
+of proportionality of conductance per area.
+
+"""
