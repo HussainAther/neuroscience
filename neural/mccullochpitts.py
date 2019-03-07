f0c4644595d768616b922b61dbcb467335f190c7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 19:16:35 -0500

    background

--- neural/mccullochpitts.py
+++ neural/mccullochpitts.py
@@ -8,5 +8,18 @@ and 1 denotes the activiated, firing state. We use the equation:
 X(t+1) = Theta(I_i - theta_i)
 
 in which t is the discretized time, theta_i is the activation threshold for unit i,
-and I_i = X_i are the data that has been identified.
+and I_i = X_i are the data that has been identified. In this case,
+
+Postsynpatic current I_iPSC = - summation of j=1 to n of (w_ij * I_j - I-i)
+
+in which w_ij is the synaptic weight of the connection from unit j to unit i,
+dependent on the synaptci gain g_ij that evolves during learning and reflecting the synaptic plasticity
+and the intracelular resistances. The capacitance is deliberately neglected and I_i is the
+externally controlled input to neuron i.
 """
+
+def mp():
+    """
+    McCulloch-Pitts model.
+    """
+    I_iPSC =  # postsynpatic current
