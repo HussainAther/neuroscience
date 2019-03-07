b53c09ae92e63fca4c98d751ef8c7f598b6ff987
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 19:12:55 -0500

    eq

--- neural/mccullochpitts.py
+++ neural/mccullochpitts.py
@@ -1,5 +1,12 @@
 import numpy as np
 
 """
-McCulloch-Pitts replaces the involved Hodgkin-Huxley system by a threshold device 
+McCulloch-Pitts replaces the involved Hodgkin-Huxley system by a threshold device
+with only two states (0 and 1) in which 0 denotes the inactivated, silent condition
+and 1 denotes the activiated, firing state. We use the equation:
+
+X(t+1) = Theta(I_i - theta_i)
+
+in which t is the discretized time, theta_i is the activation threshold for unit i,
+and I_i = X_i are the data that has been identified.
 """
