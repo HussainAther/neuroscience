9270716344d8ce2d36740d5f7857606b9642248b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 19:19:27 -0500

    working on sum

--- neural/mccullochpitts.py
+++ neural/mccullochpitts.py
@@ -13,13 +13,16 @@ and I_i = X_i are the data that has been identified. In this case,
 Postsynpatic current I_iPSC = - summation of j=1 to n of (w_ij * I_j - I-i)
 
 in which w_ij is the synaptic weight of the connection from unit j to unit i,
-dependent on the synaptci gain g_ij that evolves during learning and reflecting the synaptic plasticity
+dependent on the synpatic gain g_ij that evolves during learning and reflecting the synaptic plasticity
 and the intracelular resistances. The capacitance is deliberately neglected and I_i is the
 externally controlled input to neuron i.
 """
 
-def mp():
+def mp(n, ):
     """
-    McCulloch-Pitts model.
+    McCulloch-Pitts model. n is number of neurons. w is a list of weights for each neuron.
     """
-    I_iPSC =  # postsynpatic current
+    I_iPSC = 0 # post-synaptic current
+    for j in range(1, n):
+        I_iPSC += w[j] * I_j
+
