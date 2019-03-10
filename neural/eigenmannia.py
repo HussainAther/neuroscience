398dda14c12f1e7d6377241440b358e11664b9e2
Syed Hussain Ather <shussainather@gmail.com>
2019-03-10 04:13:46 -0400

    max

--- neural/eigenmannia.py
+++ neural/eigenmannia.py
@@ -12,3 +12,4 @@ x = loadmat("fish.mat") # read in the MATLAB data using scipy's function loadmat
 len(x["stim"]) # number of spikes in the whole experiment.
 x["__header__"] # view info about the MATLAB file.
 x["rho"] # view the rho value for each experiment.
+ max(x["stim"]) # max value of the stimulus.
