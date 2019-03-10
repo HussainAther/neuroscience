06dc0ef042ebd7f969b270fbe7a0019746541b46
Syed Hussain Ather <shussainather@gmail.com>
2019-03-10 04:52:34 -0400

    comment

--- neural/filters.py
+++ neural/filters.py
@@ -1,4 +1,10 @@
 
+"""
+We makea a simple Python simulation of Adaptive Line Enhancement using a single
+sinusoid at normalized frequency plus additive white Gaussian noise 
+"""
+
+
 def lms_ale(SNR,N,M,mu,sqwav=False,Nfft=1024):
  """
  lms_ale lms ALE adaptation algorithm using an IIR filter.
