007fce2d26d9f94ded384b49da6d8004c2884961
Syed Hussain Ather <shussainather@gmail.com>
2019-03-10 03:59:38 -0400

    bg

--- neural/eigenmannia.py
+++ neural/eigenmannia.py
@@ -1,4 +1,8 @@
 from scipy.io import loadmat
 
 """
+We're going to use data from experiments described in R. Wessel, C. Koch, and F. Gabbiani, Coding of
+time-varying electric field amplitude modulations in a wave-type electric fish. J Neurophysiol 75:2280-93 (1996). The
+weakly electric fish Eigenmannia has a special organ that generates an oscillating electric field with a frequency of
+several hundred Hz. It also has an electrosensory organ, with which it is able to sense electric fields.
 """
