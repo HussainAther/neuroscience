3eed91ac2571f9a46539b442fb13b4574cbaf4b9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-10 19:37:57 -0400

    finish gamma

--- spike/poisson.py
+++ spike/poisson.py
@@ -3,6 +3,7 @@ from numpy.matlib import *
 from numpy.linalg import *
 from matplotlib.pyplot import *
 from matplotlib.delaunay.triangulate import Triangulation
+from math import factorial
 
 """
 Poisson process between vectors A and b to see if there is some event that occurs in both.
@@ -104,15 +105,26 @@ Fano factor describes relationship between mean spike count over a given interva
 count variance.
 """
 
-def gamma(alpha, product=1):
-    """
-    Gamma function for some number alpha.
-    """
-    if alpha == 1/2:
-        return product * np.sqrt(np.pi)
-    if alpha % 2 == 0:
-        
+a = [ 1.00000000000000000000, 0.57721566490153286061, -0.65587807152025388108,
+         -0.04200263503409523553, 0.16653861138229148950, -0.04219773455554433675,
+         -0.00962197152787697356, 0.00721894324666309954, -0.00116516759185906511,
+         -0.00021524167411495097, 0.00012805028238811619, -0.00002013485478078824,
+         -0.00000125049348214267, 0.00000113302723198170, -0.00000020563384169776,
+          0.00000000611609510448, 0.00000000500200764447, -0.00000000118127457049,
+          0.00000000010434267117, 0.00000000000778226344, -0.00000000000369680562,
+          0.00000000000051003703, -0.00000000000002058326, -0.00000000000000534812,
+          0.00000000000000122678, -0.00000000000000011813, 0.00000000000000000119,
+          0.00000000000000000141, -0.00000000000000000023, 0.00000000000000000002]
 
+def gamma(n, product=1):
+    """
+    Gamma function for some number n.
+    """
+    y = float(n) - 1.0;
+    sm = a[-1];
+    for an in a[-2::-1]:
+        sm = sm * y + an;
+    return 1.0 / sm;
 
 def interspikeInterval(x, alpha, beta):
     """
