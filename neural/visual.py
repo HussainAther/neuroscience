b1b511dc844e3eaa582254cc06caddb94684f339
Syed Hussain Ather <shussainather@gmail.com>
2019-03-10 04:47:23 -0400

    comment

--- neural/visual.py
+++ neural/visual.py
@@ -5,8 +5,10 @@ np.random.seed(0)
 
 """
 We can use Gaussians in one dimension to model spatial receptive fields
-with center-surround structure.
+with center-surround structure. We use built-in python libraries to show the Gaussian
+integrals.
 """
+
 x = loadmat("ganglion.mat") # read in the MATLAB data using scipy's function loadmat.
 
 list(x) # show all column names
@@ -19,3 +21,7 @@ x["stim"][len(x)/2:] # second half of experiments
 x["stim"][:len(x)/2] # first half of experiments
 np.var(x["stim"]) # variance
 
+# Random Gaussians
+gauss_1 = norm(loc=-5,scale=5)
+gauss_2 = norm(loc=8,scale=3)
+gauss_3 = norm(loc=1.5,scale=1)
