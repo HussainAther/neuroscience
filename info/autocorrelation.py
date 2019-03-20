e8bb1ab5c87027126b5e47895528a5052ad125d2
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 21:51:49 -0400

    comment and code

--- info/autocorrelation.py
+++ info/autocorrelation.py
@@ -2,3 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+"""
+We use the autocorrelation of a time series function to look for repeating patterns
+or serial correlation (correlation between signal at a given time and a later time).
+"""
+
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    return result[result.size // 2:]
