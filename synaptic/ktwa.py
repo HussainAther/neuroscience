dde74233659cae7876bdca6aad2246cbea15d4cf
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 16:18:16 -0400

    begin var

--- synaptic/ktwa.py
+++ synaptic/ktwa.py
@@ -1,6 +1,11 @@
+import numpy as np
 
 """
 We use the k-winners-take-all (kWTA) form of inhibition that uses
 a simple set of approximations to the net effects of the inhibitory interneurons (in a
-sort of "winner takes all" approach). 
+sort of "winner takes all" approach). We capture the "set point" nature of inhibitory
+feedback as analogous to the desired temperature for the  unit.
 """
+
+theta = # action potential threshold at which a neuron will fire an action
+        # potential ooutput to another neuron
