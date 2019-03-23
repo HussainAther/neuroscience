f5c216b818a8472f20e0daad90b072e2525d4c0e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 13:05:57 -0400

    bg

--- neural/wilsoncowan.py
+++ neural/wilsoncowan.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 """
-
+The Wilson-Cowan model describes dynamics of interactions between simple exchitatory
+and inhibitory model neurons. It's used in modeling neuronal popuatlions with phase
+plane methods and numerical solutions to describe the responses of neuronal populations
+to stimuli.
 """
