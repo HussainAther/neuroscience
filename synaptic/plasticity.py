944315d2debe147ed07b723155e513f249013bb1
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 14:49:46 -0400

    bg

--- synaptic/plasticity.py
+++ synaptic/plasticity.py
@@ -1,4 +1,7 @@
 
 """
-Synaptic plasticity.
+Synaptic plasticity with Hebbian Learning. Using neurons of the brain as processing units
+using cable theory as extensions, we can simulate models of neurons firing using action potentials
+that deliber chemical siganls (neurotransmitters) as ion channels open and close to allow
+ions to flow.
 """
