b4973e2f0b5871ee7c339799b40d73e8248ca3bc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 15:10:05 -0400

    scale

--- synaptic/plasticity.py
+++ synaptic/plasticity.py
@@ -1,4 +1,6 @@
+import numpy as np
 
+from sklearn import make_blobs
 """
 Synaptic plasticity with Hebbian Learning. Using neurons of the brain as processing units
 using cable theory as extensions, we can simulate models of neurons firing using action potentials
@@ -19,6 +21,28 @@ x for a particular input value. This rule is unstable because when expressed usi
 we solve for the eigenvalues of C (a positive semidefinite matrix), we get eigenvectors that increase
 exponentially with t. This will cause an overflow with x and y values greater than 1.
 
-Finnish computer scientist Erkki Oja proposed an alternative
-Like Hebb's rule, we use the vector w that converges
+Finnish computer scientist Erkki Oja proposed an alternative. Like Hebb's rule, we use the vector w that converges
+to the C eigenvector, but, in this case, to a finite (small) number.
+
+Δw_i = α y(x_i - w_iy)
 """
+
+# create the dataset
+X, y =  make_blobs(n_samples=500, centers=2, cluster_std=5.0, random_state=1000)
+
+def scale(x):
+    """
+    Transform dataset x such that it will have a mean value 0 and
+    standard deviation 1.
+    """
+    a = np.mean(x) # find the mean/average of the dataset
+    s = np.std(x) # find the standard deviation of the dataset
+    r = [] # output result list
+    for i in x:
+        r.append((i-a)/s)
+    return r
+
+# scale the dataset
+Xs = scale(X)
+
+
