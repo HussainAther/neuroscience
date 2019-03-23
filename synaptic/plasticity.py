aeb9e035da0bc85da8284a7bd9f84a5c551b3c73
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 15:01:26 -0400

    hebb

--- synaptic/plasticity.py
+++ synaptic/plasticity.py
@@ -4,4 +4,21 @@ Synaptic plasticity with Hebbian Learning. Using neurons of the brain as process
 using cable theory as extensions, we can simulate models of neurons firing using action potentials
 that deliber chemical siganls (neurotransmitters) as ion channels open and close to allow
 ions to flow.
+
+"Neurons that fire together wire together."
+With Hebbian theory, we can calculate the synaptic weight from a neuron if both pre-synaptic
+and post-synaptic units behave in the same way (whether they're firing or in steady state).
+Hebbian learning uses principal component analysis by extracting the first principal component.
+Hebb's rule can be expressed as:
+
+Δw_i = α x_i y
+
+
+in which we use w as the weight, α as the learning rate, x as each value, and y as the linear combination of w and
+x for a particular input value. This rule is unstable because when expressed using vectors (Δw = αC . w), when
+we solve for the eigenvalues of C (a positive semidefinite matrix), we get eigenvectors that increase
+exponentially with t. This will cause an overflow with x and y values greater than 1.
+
+Finnish computer scientist Erkki Oja proposed an alternative
+Like Hebb's rule, we use the vector w that converges
 """
