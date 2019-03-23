19c2b3104d3a82e6fda77c1fd92db8c7b170e08d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 15:59:24 -0400

    initial commit

---
+++ synaptic/ktwa.py
@@ -0,0 +1,6 @@
+
+"""
+We use the k-winners-take-all (kWTA) form of inhibition that uses
+a simple set of approximations to the net effects of the inhibitory interneurons (in a
+sort of "winner takes all" approach). 
+"""
