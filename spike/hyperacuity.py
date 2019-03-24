82cacc928cf0ac42793e17e7f02bff434cc5ca90
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 18:14:54 -0400

    bg

--- spike/hyperacuity.py
+++ spike/hyperacuity.py
@@ -2,5 +2,13 @@ import numpy as np
 
 """
 In some cases of studying the reliability of neural computation,
-we find ourselves 
+we find ourselves at the extremes of what we can compute and determine
+about the neural functions themselves. What is the scale of spatial precision?
+We know the eye samples the world with a discrete lattice of photoreceptors.
+In the human fovea this lattice spacing on the retina corresponds to an angular spacing of ~0.01 
+degrees in the visual world.
+
+In an acuity task, we distinguish one point source from two point sources of the 
+same total intensity. We look at the intensity pattern on the retina from a single
+point source.
 """
