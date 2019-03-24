54eec769adb1672631086ca56c4d08b18356f3b5
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 18:17:46 -0400

    intensity

--- spike/hyperacuity.py
+++ spike/hyperacuity.py
@@ -12,3 +12,15 @@ In an acuity task, we distinguish one point source from two point sources of the
 same total intensity. We look at the intensity pattern on the retina from a single
 point source.
 """
+
+def I0(x):
+    """
+    This function represents a single intensity pattern we observe on the retina.
+    """
+    return np.sin(x)
+
+# for some distance l that separates the point source from the retina
+# and x as the distance between the points on the retina itself.
+x = 5
+l = 20
+I = (1/2)*(I0(x-l/2) + I0(x+ l/2) # we add each half of the intensity into a total intensity.
