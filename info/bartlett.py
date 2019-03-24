3dc51df0aa89458709f0dd795c55cf4dee983858
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 19:00:19 -0400

    initial commit

---
+++ info/bartlett.py
@@ -0,0 +1,4 @@
+"""
+In time series analysis, we use Bartlett's method (or the method of averaged periodograms)
+to estimate the power spectral density (which gives us the noise spectrum for an input spike train).
+"""
