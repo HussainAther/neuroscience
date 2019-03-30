c38236073da6d7688fd3075a42548a47a55b3f32
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 22:04:20 -0400

    spacing

--- channel/calcium.py
+++ channel/calcium.py
@@ -5,18 +5,18 @@ from Ch10_3_model import *
 Calcium parabolic bursting
 """
 
-
 gentype = 'radau' # Radau C-based integrator
 
 par_args = {'gr': 26, 'gc': 3.25, 'gx': 1.93,
             'tau_r': 5.6, 'tau_c': 125, 'tau_x': 300, 'tau_v': 0.97,
                 'Ena': 0.48, 'Er': -0.95, 'Ec': -0.95, 'Ex': 1.4,
                 'Iapp': 1.5, 'As': 0, 'f': 50, 'An': 0.0}
-ic_args = {'v': -0.82, 'r': 0.4, 'x': 0.04, 'c': 0}
 
+ic_args = {'v': -0.82, 'r': 0.4, 'x': 0.04, 'c': 0}
 
 HH = makeHHneuron('HH_Ca', par_args, ic_args, const_I=True,
                   gentype=gentype)
+
 HH.set(tdata=[0,1500])
 
 def test_I(I):
