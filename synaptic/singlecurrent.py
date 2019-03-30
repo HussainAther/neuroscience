b876c36ae75a13c237e8efcde71a2c76e0e2d60c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 17:43:31 -0400

    typo comment

--- synaptic/singlecurrent.py
+++ synaptic/singlecurrent.py
@@ -11,15 +11,18 @@ m = GEKKO()
 The sign of a synaptic action can be predicted by knowledge of the relationship between resting potential (V) and
 reversal potential (E). WE can determine amplitude between synpatic conductance and the extra synaptic
 conductances. Creating equivalent electrical cirucits we can move from a single-channel conductacne to that of macroscopic
-conductances and currents. We can determine the total conductance change produced by simultaneous activvation of many
+conductances and currents. We can determine the total conductance change produced by simultaneous activation of many
 synpases. Gamma is the single-channel conductance, P is the probabilty of opening a single channel, and N is the total
 number of ligand-gated channels in the post-synaptic conductacne
 """
 gamma = 5
 P = 10
 N = 15
-# function that returns total conductance
+
 def total_conductance(gamma, P, N)
+    """
+    Return the total conductance by simultaneous activation of many synapses.
+    """
     g_syn = gamma*P*N # Add conductances in parallel for simultaneous activation
     return g_syn
 
