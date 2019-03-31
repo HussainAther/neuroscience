a7a61de6db5e10db3d722965418da356f064cfee
Syed Hussain Ather <shussainather@gmail.com>
2019-03-31 00:33:13 -0400

    typo

--- spike/poisson.py
+++ spike/poisson.py
@@ -12,7 +12,7 @@ Poisson's equation is obtained from adding a source term to the right-hand-side 
 
 ∂2p/∂x2+∂2p/∂y2 = b
 
-So, unlinke the Laplace equation, there is some finite value inside the field that affects the solution.
+So, unlike the Laplace equation, there is some finite value inside the field that affects the solution.
 Poisson's equation acts to "relax" the initial sources in the field.
 """
 
