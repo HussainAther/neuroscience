79f6450697bf8821a1dc2543b29621dcc16b33af
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 20:32:27 -0400

    bg

--- synaptic/hopfield.py
+++ synaptic/hopfield.py
@@ -21,7 +21,9 @@ f_A(x) = 1 if summation from j=1 to N of w_ij*x_j > θ_i
 
 We can define hte energy of a Hopfield network as :
 
-E = (-1/2)*(summation of i)(summation of j) of w_ij*x_i*x_j + (summation of i) of θ_i * x_i 
+E = (-1/2)*(summation of i)(summation of j) of w_ij*x_i*x_j + (summation of i) of θ_i * x_i
+
+We're going to use Hopfield networks to improve the pattern performance of a few 5x5 grids patterns. 
 """
 
-
+p = 4
