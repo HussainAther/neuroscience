2c2b8e148fa999e12fbd23e8c524b4ade12b2259
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 20:17:26 -0400

    bg

--- synaptic/hopfield.py
+++ synaptic/hopfield.py
@@ -6,5 +6,12 @@ dynamics to construct an energy function in which the stored memory states
 were local minima of the energy function. It allowed Hebbian learning to lead
 to attrcator memories and strengthened the bridge between memory networks
 and certain branches of statistical mechanics.
+
+Hopfield networks are recurrent neural networks with bipolar threshold neurons.
+They're used in associative memory. The synaptic weights have the following conditions:
+
+w_ij = wji ∀ i,j ∈ {1,...,N}
+w_ii = 0 ∀ i ∈ {1,...,N}
 """
+
 
