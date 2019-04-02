2fabe9b7cf7803c89386e0758381d26104bb4989
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 16:39:34 -0400

    return

--- neural/synchrony.py
+++ neural/synchrony.py
@@ -13,6 +13,9 @@ def crosscorrelation(tau, x, n):
     For some time lag tau derived from normalized signals x and y of length N and with zero mean, we
     can calculate the unit variance as linear cross correlation and use that as a measure of synchronization.
     """
+    if tau < 0:
+        tau = -tau # flip the sign so we're looking at time lag in the same direction every time we run this function
     summ = 0
     for i in range(N-tau):
         summ += x[i+tau] * y[n] # account for time lag tau
+    return (1/(N-tau)) * summ
