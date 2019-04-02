a813784fa5f56368c70fc90bfec5d117421054a9
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 16:38:25 -0400

    start the func

--- neural/synchrony.py
+++ neural/synchrony.py
@@ -8,8 +8,11 @@ series and high values for correlated time series.
 We can look at both cross-correlation and coherence measures of this synchrony.
 """
 
-def crosscorrelation(tau):
+def crosscorrelation(tau, x, n):
     """
     For some time lag tau derived from normalized signals x and y of length N and with zero mean, we
     can calculate the unit variance as linear cross correlation and use that as a measure of synchronization.
     """
+    summ = 0
+    for i in range(N-tau):
+        summ += x[i+tau] * y[n] # account for time lag tau
