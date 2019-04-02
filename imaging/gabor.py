8edd09925fb5db4906ae9ad9e49de43f0c06618f
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 15:16:33 -0400

    bg

--- imaging/gabor.py
+++ imaging/gabor.py
@@ -6,5 +6,8 @@ It detects frequency content in the image in specific directions in a localized
 region around the point or region of analysis.
 
 We can investigate the extent to which it's similar to perception of the human visual system
-with biologically plausible deep learning techniques.
+with biologically plausible deep learning techniques. If we were to fix hidden layer weights
+of a neural network with random Gabor filters or train them with unsupervised methods (such as 
+principal component analysis), we can determine whether unsupervised learning leads to better
+performanance than networks that are completely connected within themselves. 
 """
