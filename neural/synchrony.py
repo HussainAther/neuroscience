9de061b383e761ebdd0c9ba6555eb169645959cb
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 16:37:20 -0400

    def cc

--- neural/synchrony.py
+++ neural/synchrony.py
@@ -8,4 +8,8 @@ series and high values for correlated time series.
 We can look at both cross-correlation and coherence measures of this synchrony.
 """
 
-
+def crosscorrelation(tau):
+    """
+    For some time lag tau derived from normalized signals x and y of length N and with zero mean, we
+    can calculate the unit variance as linear cross correlation and use that as a measure of synchronization.
+    """
