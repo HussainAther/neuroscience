5123320a293be31100e9c2d3812cc94ee7b025f9
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 16:35:57 -0400

    initial commit

---
+++ neural/synchrony.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+"""
+Linear measures of neuronal signal synchrony estimate the synchrony between two or sometimes
+more continuous time series of brain activitiy which yield low values for independent time
+series and high values for correlated time series. 
+
+We can look at both cross-correlation and coherence measures of this synchrony.
+"""
+
+
