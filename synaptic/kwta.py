8b92e904d1e9259511b8281fbd58251b6b29916c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-04 17:41:19 -0400

    result code

--- synaptic/kwta.py
+++ synaptic/kwta.py
@@ -8,8 +8,12 @@ feedback as analogous to the desired temperature for the  unit.
 """
 
 l = range(10) # original list of values
+k = .5 # some arbitrary k
 
 theta = 10 # action potential threshold at which a neuron will fire an action
         # potential output to another neuron
 
-
+j = int(len(l)*k) # number of elements whose maximum we want 
+threshold = sorted(l)[j] # the jth element in the sorted list.
+result = np.zeros(len(l)) # initialize zero array 
+result = [int(x>=threshold) for x in l] # each list member that is above or equal to the threshold is 1. All others remain 0.
