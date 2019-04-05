69bcd0ef3122d139699b9108697a292997d29bfa
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 15:05:01 -0400

    bg

--- neural/lptc.py
+++ neural/lptc.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 """
-We model Lobula plate tangential cells (LPTC lptc) used in the fly visual ganglia. 
+We model Lobula plate tangential cells (LPTC lptc) used in the fly visual ganglia. The lobula plate is
+in the posterior part of the lobula complex (the third visual ganglion of the fly eye). We use electrophysiological
+recordings to study the large diameter of the intracellular processes. The integrating neurons can be modeled on a 
+biophysical level with direct recordings of their membrane properties. 
 """
