29e0fa4972a5fce2ee95058768e3eebced0f5ef5
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 16:40:44 -0400

    header spacing

--- synaptic/singlecurrent.py
+++ synaptic/singlecurrent.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+
+from scipy.integrate import odeint
 from gekko import GEKKO
 
 # Startup GEKKO for differential equations
