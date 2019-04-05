e34d8f6cf09834645471c7c10c2f09e9913ab8ba
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 12:34:23 -0400

    spacing

--- neural/eigenmannia.py
+++ neural/eigenmannia.py
@@ -1,12 +1,13 @@
-from scipy.io import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
+
+from scipy.io import loadmat
 
 """
 We're going to use data from experiments described in R. Wessel, C. Koch, and F. Gabbiani, Coding of
 time-varying electric field amplitude modulations in a wave-type electric fish. J Neurophysiol 75:2280-93 (1996). The
 weakly electric fish Eigenmannia has a special organ that generates an oscillating electric field with a frequency of
-several hundred Hz. It also has an electrosensory organ, with which it is able to sense electric fields.
+several hundred Hz. It also has an electrosensory organ with which it is able to sense electric fields.
 """
 
 x = loadmat("fish.mat") # read in the MATLAB data using scipy's function loadmat.
