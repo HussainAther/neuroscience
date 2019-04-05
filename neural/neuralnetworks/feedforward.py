fc42016f1a190294fb492f3c978498b4567d08e9
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 12:17:56 -0400

    typo

--- neural/neuralnetworks/feedforward.py
+++ neural/neuralnetworks/feedforward.py
@@ -8,9 +8,9 @@ random.seed(int(time.time())) # some random seed lol
 A feedforward neural network is an artificial neural network wherein
 connections between the nodes do not form a cycle. As such, it is
 different from recurrent neural networks. The feedforward neural
- network was the first and simplest type of artificial neural network devised.
+network was the first and simplest type of artificial neural network devised.
  
- We'll create one with backpropogation
+We'll create one with backpropagation, the backward propogation of errors. 
 """
 
 # sigmoid activation function
