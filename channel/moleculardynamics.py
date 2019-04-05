64297188b83c2df07e690ef10bb9b287d2b6af62
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 13:54:06 -0400

    bg

--- channel/moleculardynamics.py
+++ channel/moleculardynamics.py
@@ -2,4 +2,6 @@ import numpy as np
 
 """
 We use the atomistic simulation technique of ion channels as a general example of molecular dyanmics.
+We use empirical potentisl to simulate interactions between all atoms in the system with an example
+potential function.
 """
