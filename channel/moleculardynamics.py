7f76781750dad9396b6c932ec9fc32761add5a3f
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 14:01:07 -0400

    more var

--- channel/moleculardynamics.py
+++ channel/moleculardynamics.py
@@ -6,15 +6,17 @@ We use empirical potentisl to simulate interactions between all atoms in the sys
 potential function.
 """
 
-def V(bonds, angles, torsions):
+def V(bonds, angles, torsions, eps, sigma, eq, qi, rij):
     """
     Example molecular dynamics potential V. We use harmonic terms for bonds and angles, a cosine expansion
     for torsion angles, and Lennard-Jones and Coulomb interactions for non-bonded interactions. bonds is the
-    list of bond lengths, angles is the list of angles, torsions are 3-piece tuple to describe the dihedral angles
+    list of bond lengths, angles is the list of angles (in degree), torsions are 3-piece tuple to describe the dihedral angles
     (rotations around a central bond), eps and sigma are Lennard-Jones parameters, eq and qi are partial atomic charges,
     and rij is the distance between the two atoms. 
     """
     bondsum = 0 # summation of bond energies
     anglesum = 0 # summation of angle energies
-    li0 = 10 # refernce bond length
-    for l in bonds: # for each bond lenght
+    li0 = 1 # reference bond length
+    thetai0 = 90 # reference bond angle
+    for l in bonds: # for each bond length
+
