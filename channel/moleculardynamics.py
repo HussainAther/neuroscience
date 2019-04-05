c3b1e03b1267b70e0824e138ce95a0070d66f8f2
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 14:09:51 -0400

    torsionsum

--- channel/moleculardynamics.py
+++ channel/moleculardynamics.py
@@ -6,19 +6,32 @@ We use empirical potentisl to simulate interactions between all atoms in the sys
 potential function.
 """
 
-def V(k, bonds, angles, torsions, eps, sigma, eq, qi, rij):
+def chunks(l, n):
+    """
+    Return successive n-sized chunks from l.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def potential(k, bonds, angles, torsions, eps, sigma, eq, qi, rij):
     """
     Example molecular dynamics potential V. We use harmonic terms for bonds and angles, a cosine expansion
     for torsion angles, and Lennard-Jones and Coulomb interactions for non-bonded interactions. k is an array of the
     harmonic force constants. bonds is the list of bond lengths, angles is the list of angles (in degree), torsions 
-    are 3-piece tuple to describe the dihedral angles (rotations around a central bond), eps and sigma are Lennard-
+    are 3-piece tuple to describe the dihedral angles (rotations around a central bond: n, V, gamma), eps and sigma are Lennard-
     Jones parameters, eq and qi are partial atomic charges, and rij is the distance between the two atoms. 
     """
     bondsum = 0 # summation of bond energies
     anglesum = 0 # summation of angle energies
+    torsionsum = 0 # summation of torsion energy due to torsion
     li0 = 1 # reference bond length
     thetai0 = 90 # reference bond angle
-    n = len(bonds) # number of inputs
-    for i in range(n): # for each input
+    m = len(bonds) # number of inputs
+    omega = chunks(range(90), m) # get evenly spaced list of 0 to 90 for the number of inputs
+    for i in range(m): # for each input
+        V = torsions[i][0] # n for dihedral angle
+        n = torsions[i][1] # omega for dihedral angle
+        gamma = torsions[i][2] # gamma for dihedral angle
         bondsum += (bonds[i] - li0)**2 *(k[i]/2)
         anglesum += (angles[i] - thetai0)**2 *(k[i]/2)
+        torsionsum += (1 + np.cos(n*omega-gamma)) * (V/2) 
