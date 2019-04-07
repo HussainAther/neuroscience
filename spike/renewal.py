fa6a1839ae6deee947c63e2a1ce2254f46b22cda
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 21:20:43 -0400

    survivor def

--- spike/renewal.py
+++ spike/renewal.py
@@ -4,3 +4,10 @@ point processes to describe a sequence of events in time. In the narrow sense, t
 input and are defined by the fact that the state of the system, and the probability of generating the
 next event depends only on the "age" (t-t_hat) of the system. 
 """
+
+def survivor(t, t_hat):
+    """
+    The survivor function defines the probability that the neuron stays quiescent between t_hat and t.
+    It's the probability a neuron "survives" from t_hat to t without firing.
+    """
+
