58f5ddb0cb5cdf9c112d5a87a40112ea88a00a3a
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 21:32:18 -0400

    laplace transform

--- spike/renewal.py
+++ spike/renewal.py
@@ -8,9 +8,16 @@ next event depends only on the "age" (t-t_hat) of the system.
 def P0(s):
     """
     Calculate the probability that the next event occurs at time t + s given that the last event time  
-    was time t.     
+    was time t. We use the Laplace transform as the Fourier transform of gamma(x)f(x)exp(b) in which
+    gamma is a step function that gets rid of the negative part of the integral and exp(bx) is the
+    real part of the complex exponential. In this case, we take the complex number form of the interval
+    s and integrate over that to return the renewal probability. 
     """
-    t  
+    com = np.complex(1, s) # complex number form of the imaginary component s
+    a = 2
+    dt = .5 # some time differential
+    f = lambda u: 1/(u-a)
+    return (f(com)).real*np.cos(com.imag*dt) * 2*np.exp(com.real*dt)/np.pi 
 
 def survivor(t, t_hat):
     """
