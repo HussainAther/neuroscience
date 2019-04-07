4012704ab2dd2f98e6f23f5d1f4f40c01414e1b7
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 21:04:33 -0400

    initial commit

---
+++ spike/population.py
@@ -0,0 +1,7 @@
+"""
+We study the population activity by taking into account the spikes of a population of neurons 
+that are sent off to another population (from m to n). The proportion of active neurons in the
+presynaptic population m can be used to define population activity.
+"""
+
+
