948ea92fef89cb64f81696cc066bb028799d76f1
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 20:29:19 -0400

    bg

--- spike/escape.py
+++ spike/escape.py
@@ -1,4 +1,10 @@
+import numpy as np
 
 """
-In the escape noise model, the integrate-and-fire neuron with a threshold (theta) that fires even though
+In the escape noise model, the integrate-and-fire neuron with a threshold (theta) that fires even though the formal
+threshold hasn't been reached or may stay quiescent even though the formal threshold has transiently been passed.
+We use the "escape rate" or "firing intensity" that depends on the momentary state of the neuron.
+This Spike Response Model (SRM) is a type of generalized integrate-and-fire model that we can calculate.
 """
+
+
