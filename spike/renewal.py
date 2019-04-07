ca490f42f7d19ba29ddbf1615737fd428d930e67
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 21:19:29 -0400

    bg

--- spike/renewal.py
+++ spike/renewal.py
@@ -1,3 +1,6 @@
 """
-We use Markov renewal processes to generalize Markov jump processes.
+We use Markov renewal processes to generalize Markov jump processes. Renewal processes are stochastic
+point processes to describe a sequence of events in time. In the narrow sense, they presuppose stationary 
+input and are defined by the fact that the state of the system, and the probability of generating the
+next event depends only on the "age" (t-t_hat) of the system. 
 """
