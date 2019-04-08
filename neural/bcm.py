22dfc4ea9819c5900272578da0f1d38cfc28339e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-07 21:44:20 -0400

    initial commit

---
+++ neural/bcm.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+"""
+During neural development, sensory stimulation induces long-term changes in the receptive 
+field of the neurons that encode the stimuli. We use hte Bienenstock-Cooper-Munro (BCM bcm) 
+model to analyze this process computationallly. 
+"""
