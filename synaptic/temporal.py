a57db978dacbabee472b3fadb6ef9e2dc6876273
Syed Hussain Ather <shussainather@gmail.com>
2019-04-07 22:15:13 -0400

    comment

--- synaptic/temporal.py
+++ synaptic/temporal.py
@@ -7,7 +7,11 @@ into the synaptic cleft. These transmitter molecules react with with ionotropic 
 metabotropic receptors which open ion channels with a postsynaptic current (PSC).
 """
 
-def IPSC():
+def IPSC(O, N, g, U, E):
     """
-    Postsynaptci current as described above.
+    Postsynaptic current as described above. O is the number of open channels, N
+    is the total number of channels, g is the channel conductance at maximum conductnace, 
+    U is the synaptic potential, and E is the reversal potential of the G protein-gated
+    potassium channels. 
     """ 
+    return 
