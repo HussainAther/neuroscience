ffd24cfa987a1676221d9b5cac1c89ba846bb926
Syed Hussain Ather <shussainather@gmail.com>
2019-04-07 22:07:42 -0400

    def

--- synaptic/temporal.py
+++ synaptic/temporal.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 """
 Each action potential at the presynaptic termianl causes the binomially distributed
 release of one or more vesicles that pour the total amount of transmitter molecules
 into the synaptic cleft. These transmitter molecules react with with ionotropic or with 
-meteabotropic receptors which open ion channels with a postsynaptic current (PSC).
+metabotropic receptors which open ion channels with a postsynaptic current (PSC).
 """
+
+def IPSC():
+    """
+    Postsynaptci current as described above.
+    """ 
