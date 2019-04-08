d5458fbffc41b56815dbe409f00dbec6b4e77ef0
Syed Hussain Ather <shussainather@gmail.com>
2019-04-07 23:19:43 -0400

    bg

--- pea.py
+++ pea.py
@@ -1,5 +1,7 @@
 import numpy np
 
 """
-We use the partition ensemble average (PEA pea) framework in reducing a system 
+We use the partition ensemble average (PEA pea) framework in reducing a system.
+At each step we introduce a transition rate from nucleation to the release
+of an MFE (multiple-firing event) to capture the dynamic nature of their emergence. 
 """
