edead47cfb9ac31d1bec46db25f9c2f56caf9bde
Syed Hussain Ather <shussainather@gmail.com>
2019-04-07 22:06:50 -0400

    initial commit

---
+++ synaptic/temporal.py
@@ -0,0 +1,6 @@
+"""
+Each action potential at the presynaptic termianl causes the binomially distributed
+release of one or more vesicles that pour the total amount of transmitter molecules
+into the synaptic cleft. These transmitter molecules react with with ionotropic or with 
+meteabotropic receptors which open ion channels with a postsynaptic current (PSC).
+"""
