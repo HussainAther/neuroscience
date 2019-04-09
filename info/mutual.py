f67462998a4193862d6b89b544085b808889e711
Syed Hussain Ather <shussainather@gmail.com>
2019-04-09 00:00:33 -0400

    joint

--- info/mutual.py
+++ info/mutual.py
@@ -5,8 +5,15 @@ We can measure how much one random variable tells us
 about another with mutual information.
 """
 
+def P(x, y):
+    """
+    Joint probability distribution P for x and y.
+    """
+     
+
 def mutI(x, y):
     """
     Mutual information between two discrete variables x and y
     with a joint probability distribution Pxy.
     """
+    
