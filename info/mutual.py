15bc2c8da1f46ca55b95116f98a8293e9e292772
Syed Hussain Ather <shussainather@gmail.com>
2019-04-09 00:10:12 -0400

    return Hx Hy

--- info/mutual.py
+++ info/mutual.py
@@ -19,14 +19,25 @@ def mutI(x, y):
     summ = 0
     for i in x:
         for j in y:
-            summ += P(i, j) * np.log(P(i, j)/i*j)
+            summ += P(i, j) * np.log(P(i, j)/i*j) 
     return summ
    
-def H(x):
+def H(x,y):
     """
     Uncertainty that (1) should be maximal when Px(x) is uniform and should
     increase with the number of possible values x can take, (2) should be 
     same if we reorder probabilities assigned to different values of x,
     (3) uncertainty about two independent random variables should be the 
     sum of hte uncertainties about each of them. 
-    """ 
+    """
+    Px = np.zeros(len(x)) # marginals
+    Py = np.zeros(len(y))
+    Hx = 0 # uncertainty of random variable x
+    Hy = 0 # of y
+    for i in range(len(y)):
+        Px += P(i, y[i])
+        Hx -= Px * np.log(P(i, y[i]))
+    for i in range(len(x)):
+        Py += P(i, x[i])
+        Hy -= Py * np.log(P(i, x[i]))
+    return Hx, Hy  
