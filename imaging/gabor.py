8f9a5e58485daaa50d64929f07308f34d887af3f
Syed Hussain Ather <shussainather@gmail.com>
2019-04-10 10:47:49 -0400

    keyword

--- imaging/gabor.py
+++ imaging/gabor.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 """
-In image processing, the Gabor filter is a linear filter for texture analysis. 
+In image processing, the Gabor (gabor) filter is a linear filter for texture analysis. 
 It detects frequency content in the image in specific directions in a localized
 region around the point or region of analysis.
 
