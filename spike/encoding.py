db47efb03c5100bb69f4510c36a9584c5817adfb
Syed Hussain Ather <shussainather@gmail.com>
2019-04-21 14:55:10 -0400

    quotes

--- spike/encoding.py
+++ spike/encoding.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from numpy import zeros
 
 """
@@ -32,8 +33,8 @@ for i in range(delay,len(stim)):
 
 plt.plot(t,stim)
 plt.plot(t,response)
-plt.xlabel('Time (s)')
-plt.legend(['Stimulus','response'])
+plt.xlabel("Time (s)")
+plt.legend(["Stimulus","response"])
 
 # Running average
 N= 10
@@ -44,8 +45,8 @@ for i in range(N, len(stim)):
 
 plt.plot(t, stim)
 plt.plot(t, response)
-plt.xlabel('Time (s)')
-plt.legend(['Stimulus','response'])
+plt.xlabel("Time (s)")
+plt.legend(["Stimulus","response"])
 
 # Leaky average
 response = np.zeros(len(stim))
@@ -60,8 +61,8 @@ for i in range(N, len(stim)):
 
 plt.plot(t, stim)
 plt.plot(t, response)
-plt.xlabel('Time (s)')
-plt.legend(['Stimulus','response'])
+plt.xlabel("Time (s)")
+plt.legend(["Stimulus","response"])
 
 # Retinal ganglion cells
 [X,Y] = np.meshgrid(np.arange(-2,2,0.1),np.arange(-2,2,0.1))
@@ -73,34 +74,34 @@ rgc = (1/(2*np.pi*sigma_c**2))*np.exp(- (X**2 + Y**2)/(2*sigma_c**2)) - (B/(2*np
 
 plt.subplot(1,2,1)
 plt.imshow(rgc)
-plt.axhline(20,color='g')
-plt.title('On center ganglion cell')
-plt.axis('off')
+plt.axhline(20,color="g")
+plt.title("On center ganglion cell")
+plt.axis("off")
 plt.subplot(1,2,2)
 plt.plot(rgc[20,:])
-plt.axis('off')
-plt.axhline(0,color='k')
-plt.title('Receptive field cross section')
+plt.axis("off")
+plt.axhline(0,color="k")
+plt.title("Receptive field cross section")
 
 # Primal visual cortex single cells
 [X,Y] = np.meshgrid(np.arange(-2,2,0.1),np.arange(-2,2,0.1))
 
-f     = 0.6     # Spatial frequency
-theta = 0.0     # Orientation
-phi   = np.pi/2 # Phase offset
-sigma = 0.3     # Standard deviation of the gaussian kernel
-gamma = 0.6     # Aspect ratio
+f = 0.6 # Spatial frequency
+theta = 0.0 # Orientation
+phi = np.pi/2 # Phase offset
+sigma = 0.3 # Standard deviation of the gaussian kernel
+gamma = 0.6 # Aspect ratio
 
 X_rot = X*np.cos(theta) + Y*np.sin(theta)
 Y_rot = -X*np.sin(theta) + Y*np.cos(theta)
 
 g = np.exp( - (X_rot**2 + (gamma**2)*(Y_rot**2))/(2*sigma**2))*np.cos(2*np.pi*f*X_rot + phi)
 plt.subplot(1,2,1)
-plt.title('Simple cell RF')
-plt.axis('off')
+plt.title("Simple cell RF")
+plt.axis("off")
 plt.imshow(g)
 plt.subplot(1,2,2)
 plt.plot(g[20,:])
-plt.title('RF cross-section')
-plt.axis('off')
+plt.title("RF cross-section")
+plt.axis("off")
 plt.axhline(0,color='k')
