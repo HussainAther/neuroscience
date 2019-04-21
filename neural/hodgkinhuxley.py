b94f89b1019c7bc08f83f1f631b821da2e5d63bf
Syed Hussain Ather <shussainather@gmail.com>
2019-04-20 22:09:42 -0400

    comments

--- neural/hodgkinhuxley.py
+++ neural/hodgkinhuxley.py
@@ -3,33 +3,19 @@ import numpy as np
 
 from scipy.integrate import odeint
 
-# Set random seed (for reproducibility)
 np.random.seed(1000)
 
 # Start and end time (in milliseconds)
 tmin = 0.0
 tmax = 50.0
 
-# Average potassium channel conductance per unit area (mS/cm^2)
-gK = 36.0
-
-# Average sodoum channel conductance per unit area (mS/cm^2)
-gNa = 120.0
-
-# Average leak channel conductance per unit area (mS/cm^2)
-gL = 0.3
-
-# Membrane capacitance per unit area (uF/cm^2)
-Cm = 1.0
-
-# Potassium potential (mV)
-VK = -12.0
-
-# Sodium potential (mV)
-VNa = 115.0
-
-# Leak potential (mV)
-Vl = 10.613
+gK = 36.0 # Average potassium channel conductance per unit area (mS/cm^2)
+gNa = 120.0 # Average sodoum channel conductance per unit area (mS/cm^2)
+gL = 0.3 # Average leak channel conductance per unit area (mS/cm^2)
+Cm = 1.0 # Membrane capacitance per unit area (uF/cm^2)
+VK = -12.0 # Potassium potential (mV)
+VNa = 115.0 # Sodium potential (mV)
+Vl = 10.613 # Leak potential (mV)
 
 # Time values
 T = np.linspace(tmin, tmax, 10000)
@@ -37,12 +23,22 @@ T = np.linspace(tmin, tmax, 10000)
 # Potassium ion-channel rate functions
 
 def alpha_n(Vm):
+    """
+    Alpha rate constant for membrane voltage.
+    """ 
     return (0.01 * (10.0 - Vm)) / (np.exp(1.0 - (0.1 * Vm)) - 1.0)
 
 def beta_n(Vm):
+    """
+    Beta rate constant for membrane voltage.
+    """ 
     return 0.125 * np.exp(-Vm / 80.0)
 
 # Sodium ion-channel rate functions
+# Alpha and beta are given for the m-th and h-th ion channels.
+# n, m, and h are dimensionless quantitites between 0 and 1 for 
+# potassium channel acitvation, sodium channel activation,
+# and sodium channel inactivation, respectively.
 
 def alpha_m(Vm):
     return (0.1 * (25.0 - Vm)) / (np.exp(2.5 - (0.1 * Vm)) - 1.0)
@@ -56,7 +52,7 @@ def alpha_h(Vm):
 def beta_h(Vm):
     return 1.0 / (np.exp(3.0 - (0.1 * Vm)) + 1.0)
 
-# n, m, and h steady-state values
+# We can calcualte the individual values n, m, and h.
 
 def n_inf(Vm=0.0):
     return alpha_n(Vm) / (alpha_n(Vm) + beta_n(Vm))
@@ -67,8 +63,10 @@ def m_inf(Vm=0.0):
 def h_inf(Vm=0.0):
     return alpha_h(Vm) / (alpha_h(Vm) + beta_h(Vm))
 
-# Input stimulus
 def Id(t):
+    """
+    Input stimulus current.
+    """
     if 0.0 < t < 1.0:
         return 150.0
     elif 10.0 < t < 11.0:
