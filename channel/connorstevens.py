6440a0c10f4057f5581222af51296bcc634fd4ad
Syed Hussain Ather <shussainather@gmail.com>
2019-04-30 12:35:19 -0400

    comment and plot

--- channel/connorstevens.py
+++ channel/connorstevens.py
@@ -73,9 +73,11 @@ for i in range(2, len(t)+1):
     
     Vm = V[i-2] * 1000 # converts voltages to mV as needed in the equations on p.224 of Dayan/Abbott
     
-    # Sodium and potassium gating variables are defined by the
-    # voltage-dependent transition rates between states, labeled alpha and
-    # beta. Written out from Dayan/Abbott, units are 1/ms.
+    """ 
+    Sodium and potassium gating variables are defined by the
+    voltage-dependent transition rates between states, labeled alpha and
+    beta. Written out from Dayan/Abbott, units are 1/ms.
+    """
     
     if Vm == -29.7:
         alpha_m = 0.38/0.1
@@ -91,9 +93,11 @@ for i in range(2, len(t)+1):
     else
         alpha_n = 0.02 * (Vm + 45.7)/(1 - exp(-0.1 * (Vm + 45.7)))
     beta_n = 0.25 * exp(-0.0125 * (Vm + 55.7))
-
-    # From the alpha and beta for each gating variable we find the steady
-    # state values (_inf) and the time constants (tau_) for each m,h and n.
+ 
+    """
+    From the alpha and beta for each gating variable we find the steady
+    state values (_inf) and the time constants (tau_) for each m,h and n.
+    """
 
     tau_m = 1e-3 / (alpha_m + beta_m) # time constant converted from ms to sec
     m_inf = alpha_m / (alpha_m + beta_m)
@@ -109,11 +113,13 @@ for i in range(2, len(t)+1):
     h[i-1] = h[i-2] + (h_inf-h[i-2]) * dt/tau_h # Update h
 
     n[i-1] = n[i-2] + (n_inf-n[i-2]) * dt/tau_n #Update n
-
-    # For the A-type current gating variables, instead of using alpha and
-    # beta, we just use the steady-state values a_inf and b_inf along with
-    # the time constants tau_a and tau_b that are found empirically
-    # (Dayan-Abbott, p. 224)
+    
+    """
+    For the A-type current gating variables, instead of using alpha and
+    beta, we just use the steady-state values a_inf and b_inf along with
+    the time constants tau_a and tau_b that are found empirically
+    (Dayan-Abbott, p. 224)
+    """
 
     a_inf = (0.0761 * exp(0.0314 * (Vm + 94.22))/(1 + exp(0.0346 * (Vm + 1.17))))^(1/3.0)
     tau_a = 0.3632*1e-3 + 1.158e-3/(1 + exp(0.0497 * (Vm + 55.96)))
@@ -138,3 +144,4 @@ for i in range(2, len(t)+1):
         V[i-1] = Vapp[i-1] # ignore the voltage integration and set V to be the applied voltage
 
 plt(t,V)
+plt.show()
