2c21ce55c55e843ec87c0e6c27cb632d14b07642
Syed Hussain Ather <shussainather@gmail.com>
2019-04-30 12:41:29 -0400

    spacing

--- synaptic/kwta.py
+++ synaptic/kwta.py
@@ -4,7 +4,7 @@ import numpy as np
 We use the k-winners-take-all (kWTA) form of inhibition that uses
 a simple set of approximations to the net effects of the inhibitory interneurons (in a
 sort of "winner takes all" approach). We capture the "set point" nature of inhibitory
-feedback as analogous to the desired temperature for the  unit. In this example, k refers to the number
+feedback as analogous to the desired temperature for the unit. In this example, k refers to the number
 of neurons that we choose to become active. 
 """
 
