a59a0fa6701c37493359aa0ee7b104877783bcdc
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 04:05:02 -0400

    more current

--- channel/pinskyrinzel.py
+++ channel/pinskyrinzel.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Pinsky-Rinzel (pinsky rinzel) model two-compartment biophysical model. It mimics a full, detailed
 model of pyramidal cells. It's related to the two-compartment abstract integrate-and-fire model.
@@ -8,6 +10,25 @@ CmdVd(t)/dt = −ILeak(Vd)−ICa(Vd,s)−IK−AHP(Vd,q)-IK-CVs(t)−Vd(t) − I/
 
 [Ca]' = -.002ICa - .0125[Ca]
 """
+
+def Ileak(V):
+    """
+    Leak current for a given potential.
+    """
+    return np.sin(V) 
+
+def INa(V, h):
+    """
+    With Hodgkin-Huxley (Hodgkin huxley) parameter h and potential, 
+    return sodium current.
+    """
+    return np.sin(V)*h
+
+def IK(V, n):
+    """
+    Potassium current for potential and n parameter.
+    """
+    return np.sin(V)*n
 
 def pr(Ca, Vd, Vs, h, n, t, s, q, c, p) 
     """
