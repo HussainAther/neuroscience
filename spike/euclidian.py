0c248e8bd13c301817fb57afa5ab6e8ea2780a86
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 19:51:27 -0400

    bg

--- spike/euclidian.py
+++ spike/euclidian.py
@@ -1,3 +1,6 @@
 """
-Euclidian spike classifier.
+Euclidian spike classifier. To quantify how well a spike train discriminates 
+stimulus patterns, one first needs to estimate the optimal stimulus feature 
+for eliciting spikes. Here, we describe the application of a Euclidian pattern 
+classifier to this problem.
 """
