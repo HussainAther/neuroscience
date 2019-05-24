9c787f71f9c3f459be15702a3bc9ca26b9cb82f9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 03:57:31 -0400

    bg

--- channel/pinskyrinzel.py
+++ channel/pinskyrinzel.py
@@ -1,6 +1,6 @@
 """
 Pinsky-Rinzel (pinsky rinzel) model two-compartment biophysical model. It mimics a full, detailed
-model of pyramidal cells.
+model of pyramidal cells. It's related to the two-compartment abstract integrate-and-fire model.
 
 CmdVs(t)/dt = −ILeak(Vs)−INa(Vs,h) − IK−DR(Vs,n) + g (Vd(t)−Vs(t))/p
 
@@ -9,4 +9,11 @@ CmdVd(t)/dt = −ILeak(Vd)−ICa(Vd,s)−IK−AHP(Vd,q)-IK-CVs(t)−Vd(t) − I/
 [Ca]' = -.002ICa - .0125[Ca]
 """
 
-def pr(Ca, Vd,  
+def pr(Ca, Vd, Vs, h, n, t, s, q, c, p) 
+    """
+    Pinksy-Rinzel model basic form for input Ca concentration, somatic membrane potential Vm,
+    membrane potential of dendritic component Vd and various other variables with ratio of 
+    membrane area of somatic component to whole cell p.
+    """
+    
+    Caa = .
