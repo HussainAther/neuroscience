77b3a0f6d50411d5c0153a15945c281befd3d85d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 19:55:28 -0400

    def

--- spike/euclidian.py
+++ spike/euclidian.py
@@ -6,5 +6,12 @@ classifier to this problem. First, the spike train x and stimulus s are binned t
 give us a max of one spike per bin. r_t takes the value of 1 if the time bin ending
 at t has a spike and the value 0 if it doesn't. Segments s_t ending at time t and
 comprising of ~100 bins prior to time t are assigned to one of two ensembles 
-P(s|r=1) and P(s|r=0) depending on whether there was a spike or not.
+P(s|r=1) and P(s|r=0) depending on whether there was a spike or not. Compute feature f
+from the means m1 and m= of the conditional distributions such taht f = m1 - m0.
 """
+
+def epc(bins):
+    """
+    For array bins of 0s and 1s corresponding to spike trains, compute the Euclidian
+    pattern classifier.
+    """
