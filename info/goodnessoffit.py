871e140caff18f7a6fb7591bbb42a302d795c7d9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 00:57:21 -0400

    Return aic

--- info/goodnessoffit.py
+++ info/goodnessoffit.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 In many data analyses we compare sets of models for a given neural spike train.
 With maximum likelihood models, we can use the Akaike information criterion (AIC 
@@ -9,3 +11,4 @@ def akaike(l, q):
     For likelihod values l that measure parameter theta and q being the dimension
     of theta, we compute AIC. 
     """
+    return -2*np.log(l) + 2*q
