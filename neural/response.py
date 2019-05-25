23ce119940f274ff5494af97701174a6cb698838
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 01:11:40 -0400

    return lowpass

--- neural/response.py
+++ neural/response.py
@@ -7,15 +7,15 @@ deltaI traveling at velocity v, the inputs are spaced by deltaphi, the temporal 
 has an amplitude and phase response A and phi, respectively. 
 """
 
-def R(A, phi, deltaI, v, lambd, lowpass=False):
+def R(A, phi, deltaI, deltaphi, v, lambd, lowpass=False):
     """
     Tme-averaged detector response R.
     When the sinusoidal waves in steady state have an additional amplitude factor A(omega),
     and phase shift phi(omega), we can multiply the respective signals and subtract the result of
     the left and right multiplier to get the time-averaged detector response for amplitude A,
-    phase response phi, contrast deltaI, velocity v, wavelength lambd, and boolean of whether
-    we're assuming it's a low-pass filter. 
+    phase response phi, contrast deltaI, input spacing deltaphi, velocity v, wavelength lambd, and 
+    boolean of whether we're assuming it's a low-pass filter. 
     """
     tau = 1 # time constant
     if lowpass == True: # If the temporal filter is a low-pass of first order
-        return deltaI**2 * (tau*2*np.pi
+        return deltaI**2 * ((tau*2*np.pi*v/lamd)/(1+tau**2*(2*np.pi*v/lamd)**2) * np.sin(2*np.pi*deltaphi/lambd) 
