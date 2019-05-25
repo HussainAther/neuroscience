519589815dd5e7c059e074ac1e940739ff92334a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 00:27:10 -0400

    bg state 5

--- channel/stochastic.py
+++ channel/stochastic.py
@@ -18,10 +18,14 @@ QCK = np.matrix([[-alpha, alpha, 0],
 
 # Next, we suppose the open channel in the CK model can be blocked by a molecule of a 
 # blocker substance with concentration xB of blocker substance. Now there are two shut
-# states and the open state has one more shut state. kposB is the rate from 1 to 4, and
+# states and the open state has one more shut state, giving us a 4th state. kposB is the rate from 1 to 4, and
 # knegB is the rate from 4 to 1.
 
 QCKnew = np.matrix([[-(alpha+kposB*xB), alpha, 0, kposB*xB],
                     [beta, -(beta+kneg), kneg, 0],
                     [0, kpos*xA, -kpos*xA, 0],
                     [knegB, 0, 0, -knegB]])
+
+# Finally we suppose that nicotinic acetylcholine receptor may have one agonist or two molecules bound
+# to the shut receptor or the open receptor. If the channel could open without a bound agonist, we'd
+# have one more state, state 5.
