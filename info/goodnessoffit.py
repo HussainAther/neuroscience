575681348ff848cd7b57e31cbfe89ec466c3958f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 00:55:59 -0400

    Def

--- info/goodnessoffit.py
+++ info/goodnessoffit.py
@@ -1,3 +1,9 @@
 """
 In many data analyses we compare sets of models for a given neural spike train.
+With maximum likelihood models, we can use the Akaike information criterion (AIC 
+criteria Akaike's).
 """
+
+def akaike(l, q):
+    """
+    """
