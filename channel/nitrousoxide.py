a2d6ba534acd75fdac84d58dca9800334f14310b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 23:50:46 -0400

    def

--- channel/nitrousoxide.py
+++ channel/nitrousoxide.py
@@ -3,5 +3,12 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 """
-Nitrous oxide (NO) diffusion from an irregular 3D structure.
+Nitrous oxide (NO) diffusion from an irregular 3D structure. One such equation, known 
+as the Crank-Nicholson scheme, is recommended for diffusive problems in one space dimension. 
 """
+
+def diffusion(d2udx2, d2udy2):
+    """
+    With diffusion coefficient D, measure diffusion over time with using
+    a two-dimensional diffusion equation.    
+    """
