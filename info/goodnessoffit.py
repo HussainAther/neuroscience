fbc84758b7d5a209dff42d6748a3ebc88068f3b2
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 00:58:18 -0400

    Goodness

--- info/goodnessoffit.py
+++ info/goodnessoffit.py
@@ -3,7 +3,7 @@ import numpy as np
 """
 In many data analyses we compare sets of models for a given neural spike train.
 With maximum likelihood models, we can use the Akaike information criterion (AIC 
-criteria Akaike's).
+criteria Akaike's) for goodness-of-fit (goodness of fit goodnessoffit).
 """
 
 def akaike(l, q):
