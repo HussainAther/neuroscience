8b3a865813961160ab4784c668922f40daf5aafa
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 01:09:09 -0400

    def bg

--- neural/response.py
+++ neural/response.py
@@ -2,5 +2,15 @@
 Steady-state (steady state) of a correlation detector for periodic sine gratings
 moving at a constant velocity. For sine waves of wavelength lambda (lamd) and contrast
 deltaI traveling at velocity v, the inputs are spaced by deltaphi, the temporal filter
-has an amplitude and phase response A and phi, respectively.
+has an amplitude and phase response A and phi, respectively. 
 """
+
+def R(A, phi, deltaI, v, lambd, lowpass=False):
+    """
+    When the sinusoidal waves in steady state have an additional amplitude factor A(omega),
+    and phase shift phi(omega), we can multiply the respective signals and subtract the result of
+    the left and right multiplier to get the time-averaged detector response for amplitude A,
+    phase response phi, contrast deltaI, velocity v, wavelength lambd, and boolean of whether
+    we're assuming it's a low-pass filter. 
+    """
+    if lowpass == True: # If the temporal filter is a low-pass of first order
