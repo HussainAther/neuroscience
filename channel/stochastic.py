969a9feaa992d4d21659d9dc3983d55e67be3e52
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 00:16:45 -0400

    states

--- channel/stochastic.py
+++ channel/stochastic.py
@@ -1,4 +1,7 @@
 """
 Stochastic models of single ion channels. Transition rate between two chemical states has dimensions 
-s^-1. 
+s^-1. If a ligand must be bound for an ion channel to open, we have three states (as Castillo and
+Katz described CK mechanism): two shut states and one open state. State 1 is open with an agonist
+molecule bound to a receptor, State 2 has a molecule bound but the channel is shut, and State 3 has
+the channel shut with receptor unoccupied. 
 """
