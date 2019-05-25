7e4d72bd8e7117065c5b8a4723c40ff9e00bc052
Syed Hussain Ather <shussainather@gmail.com>
2019-05-25 01:06:40 -0400

    bg

--- neural/response.py
+++ neural/response.py
@@ -1,3 +1,6 @@
 """
-Steady-state (steady state)
+Steady-state (steady state) of a correlation detector for periodic sine gratings
+moving at a constant velocity. For sine waves of wavelength lambda (lamd) and contrast
+deltaI traveling at velocity v, the inputs are spaced by deltaphi, the temporal filter
+has an amplitude and phase response A and phi, respectively.
 """
