8e27bf2095a48dabbdf590b1d50e4cb3507cd6a7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 03:31:43 -0400

    rename

---
+++ info/dcm.py
@@ -0,0 +1,10 @@
+
+
+"""
+Dynamic causal modelling (DCM) concerns the relation existing between cognitive functions and their 
+neurobiological “signature” (the spatio-temporal properties of brain activity) requires an understanding 
+of how information is transmitted through brain networks. The ambition here is to ask questions such as: 
+"what is the nature of the information that region A passes on to region B"? This stems from the notion 
+of functional integration, which views function as an emergent property of brain networks. Dynamic causal 
+modelling or DCM was developed specifically to address this question.
+"""
