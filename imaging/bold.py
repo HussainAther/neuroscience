92b1366016d5a31cd3f6135f244729bf0cadb359
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 19:26:17 -0400

    init commit

---
+++ imaging/bold.py
@@ -0,0 +1,11 @@
+"""
+Extracting the average time-series from one signal, time-locked to the occurence of some type of event in 
+another signal is a very typical operation in the analysis of time-series from neuroscience experiments. 
+Therefore, we have an additional example of this kind of analysis in Auditory processing in grasshoppers
+
+The following example is taken from an fMRI experiment in which a subject was viewing a motion stimulus, 
+while fMRI BOLD was recorded. The time-series in this data set were extracted from motion-sensitive voxels 
+near area MT (a region containing motion-sensitive cells) in this subject’s brain. 6 different kinds of trials 
+could occur in this experiment (designating different directions and locations of motion). The following example 
+shows the extraction of the time-dependent responses of the voxels in this region to the different stimuli.
+"""
