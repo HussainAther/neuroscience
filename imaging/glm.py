f3671e4ea054016ee1489f122c6400fd82a346c4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 05:33:07 -0400

    find files

--- imaging/glm.py
+++ imaging/glm.py
@@ -20,3 +20,17 @@ if os.path.exists("./fMRI_data") == False:
     
 open("./fMRI_data/data.zip", "wb").write(data.content)
 
+# Unzip the file
+zip_ref = zipfile.ZipFile("./fMRI_data/data.zip", "r")
+zip_ref.extractall("./fMRI_data/")
+zip_ref.close()
+
+# Find all files in the structural data folder
+data_path = "./fMRI_data/sM00223/"
+files = os.listdir(data_path)
+
+# Read in the data
+data_all = []
+for data_file in files:
+    if data_file[-3:] == "hdr":
+        data = nibabel.load(data_path + data_file).get_data()
