74a363358c427df0135e1212de49be9a90467eb8
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 19:40:57 -0400

    plot

--- info/signaltonoise.py
+++ info/signaltonoise.py
@@ -26,3 +26,14 @@ fig_tseries = []
 for idx, noise in enumerate([1, 10, 50, 100]):
     sample.append(np.ones((n_trials, ar_seq.shape[-1])) + ar_seq)
     n_points = sample[-1].shape[-1]
+for trial in  range(n_trials):
+    sample[-1][trial] += np.random.randn(sample[-1][trial].shape[0]) * noise
+sample_mean = np.mean(sample[-1], 0)
+
+fig_tseries.append(plt.figure())
+ax = fig_tseries[-1].add_subplot(1, 1, 1)
+ax.plot(sample[-1].T)
+ax.plot(ar_seq, "b", linewidth=4)
+ax.plot(sample_mean, "r", linewidth=4)
+ax.set_xlabel("Time")
+ax.set_ylabel("Amplitude")
