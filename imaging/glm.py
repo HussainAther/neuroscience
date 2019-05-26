6d497baed559330cc758d4daf82b32d065c5bcea
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 05:32:08 -0400

    load data

--- imaging/glm.py
+++ imaging/glm.py
@@ -9,3 +9,14 @@ import nibabel
 """
 Generalized linear model (GLM general) for fMRI.
 """
+
+# Define the URL of the data and download it using the Requests libary
+url = "http://www.fil.ion.ucl.ac.uk/spm/download/data/MoAEpilot/MoAEpilot.zip"
+data = requests.get(url)
+
+# Check if the targed folder for storing the data already exists. If not create it and save the zip file.
+if os.path.exists("./fMRI_data") == False:
+    os.mkdir("fMRI_data")
+    
+open("./fMRI_data/data.zip", "wb").write(data.content)
+
