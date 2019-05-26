ff71ff2f0b2ca321d691a0062ead4bd957d15e0c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 05:31:24 -0400

    import

--- imaging/glm.py
+++ imaging/glm.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import requests
+import zipfile
+import pandas as pd
+import nibabel
 
 """
 Generalized linear model (GLM general) for fMRI.
