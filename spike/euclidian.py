44eaf824837a8731114d291cac2f03b8120fa49c
Syed Hussain Ather <shussainather@gmail.com>
2019-06-28 11:31:10 -0400

    diff spelling

--- spike/euclidian.py
+++ spike/euclidian.py
@@ -1,5 +1,5 @@
 """
-Euclidian spike classifier. To quantify how well a spike train discriminates 
+Euclidian (euclidean) spike classifier. To quantify how well a spike train discriminates 
 stimulus patterns, one first needs to estimate the optimal stimulus feature 
 for eliciting spikes. Here, we describe the application of a Euclidian pattern 
 classifier to this problem. First, the spike train x and stimulus s are binned to
