8bb07e52998f0bac3014bd6b4e7d1660ea76a643
Syed Hussain Ather <shussainather@gmail.com>
2019-07-05 16:05:43 -0400

    inject

--- neural/plotneuron.py
+++ neural/plotneuron.py
@@ -7,4 +7,8 @@ class Neuron():
         self.C = .281  # Capacitance
         self.gL = .030 
         self.vR = -60.6 
-        
+        self.vT = -50.4
+    
+    def createInjectionCurrent(self):
+        self.currentInj = np.append(np.zeros(10), np.arange(100)/100)
+        self.T = len(self.currentInj)  
