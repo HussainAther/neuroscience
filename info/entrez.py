020dfa28895707195657bdd9f08a8ad093383ce4
Syed Hussain Ather <shussainather@gmail.com>
2019-07-05 19:55:04 -0400

    init var

--- info/entrez.py
+++ info/entrez.py
@@ -10,4 +10,6 @@ Search Entrez for disease information. Define search term,
 define character of interest, and return maximum nubmer of publications.
 """
 
-
+searchterm = "psychopathy"
+COI = ":" # character of interest
+maxReturn = 10000
