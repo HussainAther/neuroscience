4dd468799a939e7e91ba207706a8450849ee4da5
Syed Hussain Ather <shussainather@gmail.com>
2019-07-05 19:53:07 -0400

    import

--- info/entrez.py
+++ info/entrez.py
@@ -1,3 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Bio import Entrez
+from collections import defaultdict
+from scipy import stats
+
 """
 Search Entrez for disease information.
 """
