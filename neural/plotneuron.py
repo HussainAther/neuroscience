c8c7c948e229fea8f2d7bdb3cc0c5b8db1ee3f4f
Syed Hussain Ather <shussainather@gmail.com>
2019-07-05 16:04:43 -0400

    begin init var

--- neural/plotneuron.py
+++ neural/plotneuron.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Neuron():
+    
+    def __init__(self):
+        self.C = .281  # Capacitance
+        self.gL = .030 
+        self.vR = -60.6 
+        
