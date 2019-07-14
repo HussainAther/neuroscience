842195136068dad20c266445fd3e90b354a966f2
Syed Hussain Ather <shussainather@gmail.com>
2019-07-14 00:51:59 -0400

    def

--- neural/goldman.py
+++ neural/goldman.py
@@ -5,3 +5,6 @@ from fractions import Fraction
 """
 Goldman-Hodgkin-Katz (goldman hodgkin katz) equation.
 """
+
+def ghk(K = 310, KO = 4, KI = 140, NaO = 150, NaI = 15, ClO = 0, ClI = 0, PK = 1, PNa = 0.333333333, PCl = 0):
+
