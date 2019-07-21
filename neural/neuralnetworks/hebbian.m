f74ad91392454709c6bdf47289be44bb84d490ee
Syed Hussain Ather <shussainather@gmail.com>
2019-07-21 17:11:43 -0400

    init

---
+++ neural/neuralnetworks/hebbian.m
@@ -0,0 +1,8 @@
+function net = hebbRNN_create_model(N, B, I, p, g, dt, tau, varargin)
+
+% net = hebbRNN_create_model(N, B, I, p, g, dt, tau, varargin)
+%
+% This function initializes a recurrent neural network for later training
+% and execution
+%
+% INPUTS:
