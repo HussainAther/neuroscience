e70d7c4a1dcc2b795f8c168ef2f916dcb7cba9e8
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:45:53 -0400

    pick it yo

--- info/rescorlawagner.m
+++ info/rescorlawagner.m
@@ -7,6 +7,7 @@ nTrials = 100;
 lrate = .3;
 rewProbs = [.7 .2];
 w = .5+zeros(nTrials+1,2);
+pPickAct1(ti) = exp(w(ti,1)) / sum(exp(w(ti,:)));
 [v1,v2] = deal(-3:.1:3);
 for vi=1:length(v2)
     p(:,vi) = exp(v1) ./ (exp(v1)+exp(v2(vi)));
