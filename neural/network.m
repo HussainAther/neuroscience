9dfa5972a53a45b429f550b1996e7961435d9b36
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:53:59 -0400

    start it up

--- neural/network.m
+++ neural/network.m
@@ -3,3 +3,6 @@ Groups of neurons acting together in networks.
 }%
 N_exc = 80; % excitatory neurons
 N_inh = 20; % inhibitory neurons
+v_i = 10; % initial velocity
+time = 100; % time interval
+neuronV = v_i + zeros(N_exc+N_inh,length(time));
