40fcc285ebc1649de091eca7352b5a87f248e305
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:57:15 -0400

    exp

--- neural/network.m
+++ neural/network.m
@@ -10,6 +10,7 @@ ti = 1; % initial time
 volt_rest = -70; % resting potential (mV)
 volt_thresh = -50; % action potential thresh. (mV)
 volt_reset = -75; % post-spike reset voltage
+
 % membrane parameters
 R_m = 10; % neuron membrane resistance (MOhm)
 tau = 10; % time constant of decay (ms)
@@ -18,6 +19,15 @@ srate = 10000; % sampling rate in Hz
 sim_dur = 1; % stimulus duration in seconds
 time = 0:1/srate:sim_dur - 1/srate;
 input = zeros(1,length(time)); input(dsearchn(time',.3):dsearchn(time',.7)) = 3;
+
+if neuronV(ti) > volt_thresh
+    r_i = volt_rest + input(ti)*R_m;
+    neuronV(ti+1) = r_i + (neuronV(ti) - r_i) ...
+                    * exp(-1000/srate/tau);
+    neuronV(ti) = volt_reset;
+    spiketimes = cat(1,spiketimes,ti);
+end
+
 spikedNeurons = neuronV(:,ti) > volt_thresh;
 neuronV(spikedNeurons,ti) = volt_reset;
 r_i = volt_rest + input(ti)*R_m;
