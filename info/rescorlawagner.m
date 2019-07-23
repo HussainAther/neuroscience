ec7809fce9f38e3e18977470109978cbbf6baab1
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:44:58 -0400

    var

--- info/rescorlawagner.m
+++ info/rescorlawagner.m
@@ -3,3 +3,8 @@ Rescorla-Wagner style models describe mechanisms that show how agents
 learn to choose the best actions on the basis of rewarding or punishing
 feedback. 
 }%
+[v1,v2] = deal(-3:.1:3);
+for vi=1:length(v2)
+    p(:,vi) = exp(v1) ./ (exp(v1)+exp(v2(vi)));
+end
+imagesc(p)
