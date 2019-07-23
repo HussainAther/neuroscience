ab77e841fdc0dea37978c5d3b5b5057d3775be5e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:42:39 -0400

    spike spike spike

--- neural/izhikevich.m
+++ neural/izhikevich.m
@@ -7,4 +7,15 @@ c = -60;
 d = 4;
 tau = .25;
 tspan = 0:tau:1000;
-T1 = zeros(size(tspan)); T1(dsearchn(tspan',200):dsearchn(tspan',800)) = 1;
+T1 = zeros(size(tspan)); 
+T1(dsearchn(tspan',200):dsearchn(tspan',800)) = 1;
+% membrane potential
+V = V + tau*(.04*V^2 + 5*V + 140 - u + T1(ti));
+u = u + tau*a*(b*V-u);
+if V > 30% there was a spike
+    VV(ti+1)=30;
+    V=c; u=u+d;
+else % there was no spike
+    VV(ti+1)=V;
+end
+uu(ti+1)=u;
