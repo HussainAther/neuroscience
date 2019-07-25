1bccd852d202784fc8129d8e08f74cb1e96d94c9
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:37:52 -0400

    integrate

--- neural/lif.m
+++ neural/lif.m
@@ -7,3 +7,13 @@ uu=0; % initial membrane voltage
 I_ext=12; % constant external input
 tspan=[0 100] % integration interval
 theta=10; % firing threshold
+
+% Integration with Euler method
+t_step=0; for it=0:100;
+    t_step=t_step+1;
+    x=uu<theta;
+    uu=x*(1-tau_inv)*uu+tau_inv*Iext;%+randn;
+    u(t_step)=uu;
+    t(t_step)=it;
+    s(t_step)=1-x;
+end
