9aef4476df0fdcfc90392876dc9fb7843ea22ba5
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:42:29 -0400

    plot

--- neural/poissonspiketrain.m
+++ neural/poissonspiketrain.m
@@ -3,3 +3,17 @@ clear; clf; hold on;
 fr_mean=15/1000;
 
 % Generate presynpatic Poisson spike trains.
+lambda=1/fr_mean;
+ns=1000;
+isi1=-lambda.*log(rand(ns,1));
+
+% Delete spikes in refractory period
+is=0; for i=1:ns;
+    if rand>exp(-isi1(i)^2/32);
+        is=is+1;
+        isi(is)=isi1(i);
+    end
+end
+
+hist(isi, 50);
+cv=std(isi)/mean(isi);
