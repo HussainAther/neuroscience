29c08ab76a9427543de5327bcda6bf8292e40688
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 05:12:12 -0400

    Ext in

--- neural/cannpathintegrate.m
+++ neural/cannpathintegrate.m
@@ -8,3 +8,8 @@ beta = .06; alpha=.0; % transfer function 1/(1+exp(-beta*(u-theta))
 sig = 5/dx;
 [ws, wa]=hebb_trace(nn, sig);
 w_inh=7*(sqrt(2*pi)*sig)^2/nn;
+
+% external input to initiate bubble, no idiothetic cue, w sym
+u0=zeros(nn,1)-10; tspan=[0, 10]; param=0;
+w=ws-w_inh;
+I_ext=zeros(nn,1); for i=-20:20; I_ext(nn/2+i)=50; end
