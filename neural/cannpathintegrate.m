11532d6d5d85d8e0c8fd3b2dc63b693bc911f70f
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 05:15:22 -0400

    Weight

--- neural/cannpathintegrate.m
+++ neural/cannpathintegrate.m
@@ -13,3 +13,9 @@ w_inh=7*(sqrt(2*pi)*sig)^2/nn;
 u0=zeros(nn,1)-10; tspan=[0, 10]; param=0;
 w=ws-w_inh;
 I_ext=zeros(nn,1); for i=-20:20; I_ext(nn/2+i)=50; end
+[t1, u1]=ode45('rnn_ode', tspan, u0, [], nn, tau_inv, dx, beta, alpha, w, I_ext);
+u0=u1(size(t1,1),:0; tspan=[10,20];
+I_ext=zeros(nn,1);
+[t2, u2]=ode45('rnn_ode', tspan, u0, [], nn, tau_inv, dx, beta, alpha, w, I_ext);
+u0=u2(size(t2,1),:); tspan=[20,40];
+w=(ws-w_inh).*(1+wa(:, :, 2));
