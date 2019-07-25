c028b7088fab49209476262d4da2f6d00712a3aa
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:52:15 -0400

    init

---
+++ neural/ann.m
@@ -0,0 +1,9 @@
+% Fully connected recurrent ANN (artificial neural network)
+clear; clf; hold on;
+
+% Network parameters
+nn=500; % nodes
+npat=50; % patterns
+dt=.01; % time step
+tau=1; % time constant
+tend=.1; % end simulation
