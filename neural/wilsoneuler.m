07deea109963fbced89563d80a85813b7cb645c6
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:23:50 -0400

    params

--- neural/wilsoneuler.m
+++ neural/wilsoneuler.m
@@ -1,1 +1,6 @@
 % Integration of Wilson model with the Euler method.
+clear; clf;
+% parameters of model; 1=K,R 2=Ca,T 3=KCa,H 4=Na
+g(1)=26; g(2)=2.25; g(3)=9.5; g(4)=1; 
+E(1)=-.95; E(2)=1.20; E(3)=E(1); E(4)=.50;
+
