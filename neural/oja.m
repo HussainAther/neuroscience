1828aa0b050f4315d3b4427653f764b3d3b64950
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:50:02 -0400

    plot

--- neural/oja.m
+++ neural/oja.m
@@ -8,4 +8,13 @@ for i:1000
     x=.05*randn(2,1); x(1)=4*x(1); % training examples
     x=rot*x; % rotation of training examples
     y=w'x; % network update
-    
+    plot(x(1), x(2), '.');
+    w=w+.1*y*(x-y*2); % training
+    w_traj(:,1)=w; % recording of weight history
+end
+
+plot(w_traj(1,:), w_traj(2,:),'r');
+plot([0 w(1)], [0 w(2)], 'k', 'linewidth', 2);
+axis([-1 1 -1 1);
+plot([-1 1], [0 0], 'k'); 
+plot([0 0], [-1 1], 'k');
