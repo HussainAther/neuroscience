edc6020fa1537aeebe1965a82033c62dc980aa38
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:36:45 -0400

    params

--- neural/lif.m
+++ neural/lif.m
@@ -1,1 +1,9 @@
 % Leaky Integrate-and-fire (IAF) neuron
+clear; clf;
+
+% parameters
+tau_inv=.1; % inverse time constant
+uu=0; % initial membrane voltage
+I_ext=12; % constant external input
+tspan=[0 100] % integration interval
+theta=10; % firing threshold
