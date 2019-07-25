121e4a4562e30bdf32ec17cc47c2245000b043d9
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:34:54 -0400

    func

--- neural/wilson.m
+++ neural/wilson.m
@@ -17,3 +17,17 @@ y0=y(size(t,1),:); param=0; I_ext=1; tspan=[0 200];
 [t, y]=ode15s('wilson_odefile', tspan, y0, [], I_ext, g, E, tau);
 
 plot(t, 100*y(:, 4));
+
+function ydot=wilson(y, t, flag, I_ext, g, E, tau)
+% odefile for wilson equations
+% parameters of the model; 1=K,R; 2=Ca,T; 3=KCA,H; 4=Na;
+    V=y(4); y3=y(1:3);
+    x0(1)=1.24 + 3.7*V + 3.2*V^2;
+    x0(2)=4.025 + 11.6*V + 8*V^2;
+    x0(3)=3*y(2);
+    x0=x0';
+    ydot=tau.*(x0-y3);
+    y3(4)=17.8+47.6*V+33.8*V^2;
+    I=g.*y3.*(y(4)-E);
+    ydot(4)=I_ext-sum(I);
+return
