ed003ea6701c6a2fb0c415813f355f855d8da89c
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 05:08:04 -0400

    init

--- neural/rnn.m
+++ neural/rnn.m
@@ -1,1 +1,6 @@
 % Recurrent neural network with ordinary differential equations (rnn ode)
+function udot=rnn(t, u, flag, nn, tau_inv, dx, beta, alpha, w, I_ext)
+    r=1./(1+exp(-beta.*(u-alpha)));
+    sum=w*r*dx;
+    udot=tau_inv*(-u+sum+I_ext);
+return
