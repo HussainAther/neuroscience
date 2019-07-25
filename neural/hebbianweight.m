a00456679db5e9fed2c5d36aeb5eb6f965592ee5
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 04:45:01 -0400

    plot

--- neural/hebbianweight.m
+++ neural/hebbianweight.m
@@ -10,3 +10,14 @@ r1=-afr.*log(rand(1,npat));
 
 w=(r2-afr)*(r1-afr)';
 w=w/npat
+
+x=-260:20:260;
+[n, x]=hist(w, x);
+n=n/sum(n)/20;
+h=bar(x, n); set(h, 'facecolor', 'none');
+
+% Fit
+a0=[0 40];
+a=lsqcurvefit('normal', a0, x, n);
+n2=normal(a, x);
+plot(x, n2, 'r');
