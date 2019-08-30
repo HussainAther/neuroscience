841208f074b9f1261a4025e76b95dbb14a9bb73f
Syed Hussain Ather <shussainather@gmail.com>
2019-08-30 12:02:33 -0400

    neuron class

--- spike/stdp.py
+++ spike/stdp.py
@@ -3,6 +3,16 @@ import numpy as np
 """
 Spike-timing dependent plasticity model for learning.
 """
+class Neuron:
+  def __init__(self, weights=0):
+    np.seterr(all="ignore")
+    self.input = 0
+    self.value = 0
+    self.output = 0
+    self.threshold = 0
+    self.fired = False
+    self.potential = 0
+    self.weights = np.array([self.init_weight(weights) for x in range(weights)], dtype='float64')
 
 class STDP(LearningMethod):
     def __init__(self):
@@ -29,4 +39,9 @@ class STDP(LearningMethod):
         """
         trace = [ 0 if x == 0 else neuron.trace[int(i)] + 1 for (i, x) in enumerate(1 - neuron.inputs)]
         return trace
-
+  
+    def setup(self, neuron):
+        """
+        Create the neuron and trace.
+        """
+        neuron.trace = np.zeros(len(neuron.inputs))
