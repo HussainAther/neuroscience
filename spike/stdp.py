9668308bf670aa1ea3551c343abcb2318ab6bc63
Syed Hussain Ather <shussainather@gmail.com>
2019-08-30 11:59:18 -0400

    update

--- spike/stdp.py
+++ spike/stdp.py
@@ -12,3 +12,13 @@ class STDP(LearningMethod):
         self.adjustment = 0.1
         self.time = 0
         self.is_setup = False
+
+    def update_weights(self, neuron, adjustment):
+        """
+        Update according to the variables. Take into account the 
+        neuron response itself.
+        """
+        adjustments = np.multiply(neuron.inputs, adjustment)
+        signed_adjustments = np.multiply(adjustments, np.sign(neuron.weights))
+        adjusted = np.add(neuron.weights, signed_adjustments)
+        return adjusted
