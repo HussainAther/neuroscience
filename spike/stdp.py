57731c005df6d1c6db36f698ec2f52a5719c33a8
Syed Hussain Ather <shussainather@gmail.com>
2019-08-30 11:57:49 -0400

    init init init

--- spike/stdp.py
+++ spike/stdp.py
@@ -3,3 +3,12 @@ import numpy as np
 """
 Spike-timing dependent plasticity model for learning.
 """
+
+class STDP(LearningMethod):
+    def __init__(self):
+        """
+        Initialize variables.
+        """
+        self.adjustment = 0.1
+        self.time = 0
+        self.is_setup = False
