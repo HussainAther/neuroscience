b51c0b4d53f746de23cb5560e0b805c81854d1ac
Syed Hussain Ather <shussainather@gmail.com>
2019-09-17 16:01:44 -0700

    def

--- spike/sta.py
+++ spike/sta.py
@@ -1,3 +1,8 @@
-"""
-Compute the spike-triggered average (STA) from a stimulus and spike-train.
-"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute(stim, rho, steps):
+    """
+    Compute the spike-triggered average (STA) from a stimulus and spike-train.
+    
+    """
