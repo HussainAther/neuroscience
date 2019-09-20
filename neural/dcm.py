727fb03eb033f5047213f536ad52f8fcd68aae7f
Syed Hussain Ather <shussainather@gmail.com>
2019-09-20 14:15:33 -0700

    connectivity matrices

--- neural/dcm.py
+++ neural/dcm.py
@@ -1,4 +1,14 @@
+import numpy as np
+
 """
 Dynamic causal modeling for inferences about functional integration
-for fMRI time series analysis.
+for fMRI time series analysis. Bilinear model of the neurodynamics and 
+extended balloon model for hemodynamics. Describe using a multivariate
+differential equation.
 """
+
+# zt is the time derivative of neuronal activity with fixed connectivity
+# A, modulation of connectivity u, external inputs B and C. Matrix A is the
+# fixed or average coupling among nodes without the exogeneous input u, matrix B
+# are the change in latent coupling for each input.
+zt = (A + np.dot(u, B) * z + C*u
