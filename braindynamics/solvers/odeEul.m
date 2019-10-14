f3356433b0227c375291daaed74ebe7bd34d60c2
Syed Hussain Ather <shussainather@gmail.com>
2019-10-13 20:12:00 -0700

    Diff eq matching python

--- braindynamics/solvers/odeEul.m
+++ braindynamics/solvers/odeEul.m
@@ -25,7 +25,7 @@
 %   SOL =
 %       x: [1xt]  time points
 %       y: [nxt]  y(t) values
-%      yp: [nxt]  y'(t) values
+%      yp: [nxt]  y"(t) values
 %
 %EXAMPLE
 %  % anonymous versions of F() function shown above.
@@ -44,40 +44,13 @@
 %AUTHORS
 %  Stewart Heitmann (2016a,2017a,2018a)
 
-% Copyright (C) 2016-2018 QIMR Berghofer Medical Research Institute
-% All rights reserved.
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions
-% are met:
-%
-% 1. Redistributions of source code must retain the above copyright
-%    notice, this list of conditions and the following disclaimer.
-% 
-% 2. Redistributions in binary form must reproduce the above copyright
-%    notice, this list of conditions and the following disclaimer in
-%    the documentation and/or other materials provided with the
-%    distribution.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.   
 function sol = odeEul(ode,tspan,y0,options,varargin)
     % The InitialStep option defines our time step
-    dt = odeget(options,'InitialStep');
+    dt = odeget(options,"InitialStep");
     if isempty(dt)
         % Default InitialStep
         dt = (tspan(end) - tspan(1))/100;
-        %warning('Step size is undefined. Using InitialStep=%g',dt);
+        %warning("Step size is undefined. Using InitialStep=%g",dt);
     end
 
     % span the time domain in fixed steps
@@ -95,10 +68,10 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
     sol.ex21tdata.varargin = varargin;
     
     % Get the OutputFcn callback.
-    OutputFcn = odeget(options,'OutputFcn');
+    OutputFcn = odeget(options,"OutputFcn");
     if ~isempty(OutputFcn)
         % initialize the OutputFcn
-        OutputFcn(tspan,sol.y(:,1),'init');
+        OutputFcn(tspan,sol.y(:,1),"init");
     end
     
     % We call OutputFcn whenever the integration time exceeds a time
@@ -117,7 +90,7 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
         % Hence the while loop.
         while ~isempty(OutputFcn) && sol.x(indx)>=tspan(tspanidx)
             % call the output function
-            status = OutputFcn(sol.x(indx),sol.y(:,indx),'');
+            status = OutputFcn(sol.x(indx),sol.y(:,indx),"");
             if status==1
                 % User has cancelled the operation.
                 sol.stats.nsteps = indx;
@@ -130,13 +103,13 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
         end
         
         % Euler step
-        sol.yp(:,indx) = ode(sol.x(indx), sol.y(:,indx), varargin{:});     % y'(t) = F(t,y(t))
-        sol.y(:,indx+1) = sol.y(:,indx) + sol.yp(:,indx) * dt;             % y(t+1) = y(t) + y'(t)*dt       
+        sol.yp(:,indx) = ode(sol.x(indx), sol.y(:,indx), varargin{:});     % y"(t) = F(t,y(t))
+        sol.y(:,indx+1) = sol.y(:,indx) + sol.yp(:,indx) * dt;             % y(t+1) = y(t) + y"(t)*dt       
         
         % Check the Euler step for overflow
         if any(~isfinite(sol.y(:,indx+1)))
-            msg = num2str(sol.x(indx),'Failure at t=%g. The numerical values are no longer finite.');
-            warning('odeEul:Overflow',msg);
+            msg = num2str(sol.x(indx),"Failure at t=%g. The numerical values are no longer finite.");
+            warning("odeEul:Overflow",msg);
             sol.stats.nsteps = indx;
             sol.stats.nfailed = 1;
             sol.stats.nfevals = tcount;
@@ -145,12 +118,12 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
     end
     
     % Complete the final Euler step
-    sol.yp(:,end) = ode(sol.x(end), sol.y(:,end), varargin{:});            % y'(t) = F(t,y(t))
+    sol.yp(:,end) = ode(sol.x(end), sol.y(:,end), varargin{:});            % y"(t) = F(t,y(t))
 
     % Check the final Euler step for overflow
     if any(~isfinite(sol.y(:,end)))
-        msg = num2str(sol.x(end),'Failure at t=%g. The numerical values are no longer finite.');
-        warning('odeEul:Overflow',msg);
+        msg = num2str(sol.x(end),"Failure at t=%g. The numerical values are no longer finite.");
+        warning("odeEul:Overflow",msg);
         sol.stats.nsteps = tcount;
         sol.stats.nfailed = 1;
         sol.stats.nfevals = tcount;
@@ -160,7 +133,7 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
     % Execute the OutputFcn for the final entry in tspan.
     if ~isempty(OutputFcn)
         % call the output function
-        status = OutputFcn(sol.x(end),sol.y(:,end),'');
+        status = OutputFcn(sol.x(end),sol.y(:,end),"");
         if status==1
             % User has cancelled the operation.
             sol.stats.nsteps = tcount;
@@ -169,7 +142,7 @@ function sol = odeEul(ode,tspan,y0,options,varargin)
             return
         end        
         % cleanup the OutputFcn
-        OutputFcn([],[],'done');
+        OutputFcn([],[],"done");
     end
         
     % Stats
