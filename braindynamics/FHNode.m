1b4d3a9595a0cfeec4fb1aebdbe37178c89f50a7
Syed Hussain Ather <shussainather@gmail.com>
2019-11-06 20:14:34 -0800

    end

--- braindynamics/FHNode.m
+++ braindynamics/FHNode.m
@@ -5,3 +5,14 @@
 % FitzHugh (1961) Impulses and physiological states in theoretical models of nerve membrane. Biophysical J. 1:445?466
 % Nagumo, Arimoto and Yoshizawa (1962) An active pulse transmission line simulating nerve axon. Proc. IRE. 50:2061?2070.
 function dY = FHNode(~,Y,a,b,tau,Iapp)
+    % extract incoming variables
+    V = Y(1);
+    W = Y(2);
+    
+    % system of equations
+    dV = V - 1/3*V^3 - W + Iapp;
+    dW = (V + a - b*W) ./ tau;
+    
+    % return
+    dY = [ dV; dW];
+end
