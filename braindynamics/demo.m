b72b2a0961a50752613d805e7adf27c3590f1130
Syed Hussain Ather <shussainather@gmail.com>
2019-11-06 20:55:07 -0800

    init conditions

--- braindynamics/demo.m
+++ braindynamics/demo.m
@@ -3,3 +3,16 @@ addpath bdtoolkit-2018b/
 % construct the model
 sys = FHN();
 
+% parameters
+%sys.pardef(5).value = 1000;     % Idur = 1000
+sys.pardef = bdSetValue(sys.pardef,'Idur',1000);
+
+% time domain
+sys.tspan = [0 200];
+
+% solver options
+sys.odeoption.RelTol = 1e-5;
+
+% initial conditions
+sys.vardef(1).value = 0;    % V=0
+sys.vardef(2).value = 0;    % W=0
