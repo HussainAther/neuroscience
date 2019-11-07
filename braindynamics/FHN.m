ebbab3667fa33feb6d509e85f37d81b48f9df6ea
Syed Hussain Ather <shussainather@gmail.com>
2019-11-06 20:27:49 -0800

    end

--- braindynamics/FHN.m
+++ braindynamics/FHN.m
@@ -1,6 +1,6 @@
 % FHN Canonical FitzHugh-Nagumo model of neural excitability
-%    V' = V - 1/3*V^3 - W + Iext
-%    tau * W' = V + a - b*W
+%    V" = V - 1/3*V^3 - W + Iext
+%    tau * W" = V + a - b*W
 %
 % Example:
 %   sys = FHN();            % Construct the system struct.
@@ -8,3 +8,21 @@
 function sys = FHN()    
     % Handle to our ODE function
     sys.odefun = @FHNode;
+    % Our ODE parameters
+    sys.pardef = [
+        struct("name","a", "value",1)
+        struct("name","b", "value",0.5)
+        struct("name","tau", "value",10)
+        struct("name","Iapp", "value",1)
+        ];
+               
+    % Our ODE variables        
+    sys.vardef = [
+        struct("name","V", "value",rand)
+        struct("name","W", "value",rand)        
+        ];
+
+    % Display panels
+    sys.panels = struct();
+     
+end 
