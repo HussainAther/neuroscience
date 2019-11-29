a92ead372c6d4becb05947d9e23d511a4c1407f8
Syed Hussain Ather <shussainather@gmail.com>
2019-11-28 22:35:45 -0800

    array

--- neural/lgn.py
+++ neural/lgn.py
@@ -4,3 +4,7 @@ import numpy as np
 Lateral geniculate nucleus (LGN) neurons for ocular dominance,
 the way cells receive more input fomr one eye. 
 """
+eps = 5 # the correlation between activity in the two populations
+        # is epsilon (eps) times as strong as the mean squared activity
+        # in each population
+C = np.array([[1, eps], [eps, 1]])
