c36c0b271b4c7056f72fa5cb5baa6e5b076f5031
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 01:30:49 -0800

    return

--- channel/shunting.py
+++ channel/shunting.py
@@ -12,6 +12,14 @@ shunting dynamics work together to solve the noise-saturation dilemma,
 it is reasonable to predict that they coevolved during evolution.
 """
 
+def shunting(A, B, I):
+    """
+    For I total input, B spontaneous decay rate of the excitation, 
+    and B number of excitable states, calculate the equilibrium value 
+    of the number of excited states.
+    """
+    return (B*I)/(A+I)
+
 def voltagerate(V, g, Vt, C):
     """
     For Vt voltage rate dependent on time, tuples V and g that are
