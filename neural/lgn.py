eea6dee228bb7c070ddf9780817ce5aa32a525cb
Syed Hussain Ather <shussainather@gmail.com>
2019-11-28 22:33:33 -0800

    import

--- neural/lgn.py
+++ neural/lgn.py
@@ -1,3 +1,6 @@
+import numpy as np
+
 """
-Lateral geniculate nucleus (LGN) neurons.
+Lateral geniculate nucleus (LGN) neurons for ocular dominance,
+the way cells receive more input fomr one eye. 
 """
