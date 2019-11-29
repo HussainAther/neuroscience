60144f52f6d2ad3bd5ca16841a54867bf6c14b65
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 01:09:22 -0800

    init

--- channel/shunting.py
+++ channel/shunting.py
@@ -1,3 +1,13 @@
 """
-Shunting equation for membrane dynamics.
+Shunting equation for membrane dynamics
+
+The Shunting equation has the form of the membrane equation on 
+which cellular neurophysiology is based. This membrane equation 
+is the voltage equation that appears in the equations of Hodgkin 
+and Huxley. In other words, the gedanken experiment shows how the 
+noise-saturation dilemma is solved by using the membrane, or shunting, 
+equation of neurophysiology to describe cells interacting in on-center 
+off-surround anatomies. Because on-center off-surround anatomy and 
+shunting dynamics work together to solve the noise-saturation dilemma, 
+it is reasonable to predict that they coevolved during evolution.
 """
