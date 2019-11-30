77963b2e73da51633168fe804b39f74711f3cab6
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 16:21:40 -0800

    work it

--- neural/stm.py
+++ neural/stm.py
@@ -1,3 +1,19 @@
+import numpy as np
+
 """
 Short-term (short term) memory equations.
 """
+
+def additive(x, i):
+    """
+    In the additive model, we add the terms, possibly nonlinear, that
+    determine the rate of change of neuronal activites, or potentials, x,
+    with a single potential term i. Return the rate of change for that
+    particular potential.
+    """
+    pd = -5*x[i] # Passive decay term using a nonlinear signal
+              # with a weight value (5 here).
+    pf = 0 # Positive feedback term summed over the individual 
+           # potentials
+    for i in range(len(x)):
+        pf +=  
