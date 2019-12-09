ff7cdeb408b43abfe3da95aec4efd9d0ae6fcad8
Syed Hussain Ather <shussainather@gmail.com>
2019-12-08 22:47:05 -0800

    mem dyn

--- neural/cpg.py
+++ neural/cpg.py
@@ -4,3 +4,6 @@ import numpy as np
 Central pattern generators (cpg) neurons for cyclic task control
 in biological systems create an oscillatory activity.
 """
+
+# Membrane dynamics
+dVdt = (Isyn(V, t) + Ipm(V, t) + Iinj - V(t)/R)/C
