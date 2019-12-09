4bb9bf66245474191b902dc9d2fa22e7adf73a15
Syed Hussain Ather <shussainather@gmail.com>
2019-12-08 23:00:27 -0800

    functions

--- neural/cpg.py
+++ neural/cpg.py
@@ -6,4 +6,15 @@ in biological systems create an oscillatory activity.
 """
 
 # Membrane dynamics
-dVdt = (Isyn(V, t) + Ipm(V, t) + Iinj - V(t)/R)/C
+def dVdt(V, t):
+    """
+    Based on the synaptic current (Isyn), resting membrane potential current
+    (Ipm), and injected current (Iinj) alongside voltage (V), time (t), membrane
+    resistance (R) and capacitance (C).
+    """
+    return (Isyn(V, t) + Ipm(V, t) + Iinj - V(t)/R)/C
+
+# Output function (firing rate)
+def gamma(V, Vo, t):
+    """
+    """
