81f1002cd8f9653818fee6c5cc71e6c969aa1669
Syed Hussain Ather <shussainather@gmail.com>
2020-01-02 11:20:13 -0800

    init

---
+++ nd/dendrite.m
@@ -0,0 +1,11 @@
+import brian2 as b2
+import matplotlib.pyplot as plt
+
+from neurodynex.cable_equation import passive_cable
+from neurodynex.tools import input_factory
+
+%{
+Dendrites and passive cable equation
+}%
+
+passive_cable.getting_started()
