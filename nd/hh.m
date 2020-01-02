35fb910a1f56b5b5f0cc2c9543a97954ea9fe507
Syed Hussain Ather <shussainather@gmail.com>
2020-01-02 13:34:57 -0800

    more current

--- nd/hh.m
+++ nd/hh.m
@@ -14,3 +14,11 @@ HH.getting_started()
 current = input_factory.get_step_current(5, 100, b2.ms, I_min *b2.uA)
 state_monitor = HH.simulate_HH_neuron(current, 120 * b2.ms)
 HH.plot_data(state_monitor, title="HH Neuron, minimal current")
+
+b2.defaultclock.dt = 0.02*b2.ms
+slow_ramp_t_end = 60  # no spike. make it shorter
+slow_ramp_current = input_factory.get_ramp_current(5, slow_ramp_t_end, b2.ms, 0.*b2.uA, 12.0*b2.uA)
+state_monitor = HH.simulate_HH_neuron(slow_ramp_current, 90 * b2.ms)
+idx_t_end = int(round(slow_ramp_t_end*b2.ms / b2.defaultclock.dt))
+voltage_slow = state_monitor.vm[0,idx_t_end]
+print("voltage_slow={}".format(voltage_slow))
