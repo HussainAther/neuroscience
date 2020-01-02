9e84aa9a396d7bda1dc848ba4b5f5f3748da0916
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 22:36:30 -0800

    note it

--- nd/lif.m
+++ nd/lif.m
@@ -48,3 +48,14 @@ plot_tools.plot_voltage_and_current_traces(state_monitor, test_current, title="e
 # Print the parameters to the console and compare with your estimates.
 # LIF.print_obfuscated_parameters(random_parameters)
 
+# Note the higher resolution when discretizing the sine wave: we specify unit_time=0.1 * b2.ms
+sinusoidal_current = input_factory.get_sinusoidal_current(200, 1000, unit_time=0.1 * b2.ms,
+                                            amplitude= 2.5 * b2.namp, frequency=250*b2.Hz,
+                                            direct_current=0. * b2.namp)
+
+# Run the LIF model. By setting the firing threshold to to a high value, we make sure to stay in the linear (non spiking) regime.
+(state_monitor, spike_monitor) = LIF.simulate_LIF_neuron(input_current=sinusoidal_current, simulation_time = 120 * b2.ms, firing_threshold=0*b2.mV)
+
+# Plot the membrane voltage.
+plot_tools.plot_voltage_and_current_traces(state_monitor, sinusoidal_current, title="Sinusoidal input current")
+print("nr of spikes: {}".format(spike_monitor.count[0]))
