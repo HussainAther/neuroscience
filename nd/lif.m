3625eaf115f934f632fb2cc1bbc6f2f07291d124
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 22:26:16 -0800

    plot and print

--- nd/lif.m
+++ nd/lif.m
@@ -18,16 +18,33 @@ MEMBRANE_RESISTANCE = 10. * b2.Mohm
 MEMBRANE_TIME_SCALE = 8. * b2.ms
 ABSOLUTE_REFRACTORY_PERIOD = 2.0 * b2.ms
 
-# create a step current with amplitude= i_min
+# Create a step current with amplitude= i_min.
 step_current = input_factory.get_step_current(
     t_start=5, t_end=100, unit_time=b2.ms,
     amplitude= i_min)  # set i_min to your value
 
-# run the LIF model.
+# Run the LIF model.
 # Note: As we do not specify any model parameters, the simulation runs with the default values
 (state_monitor,spike_monitor) = LIF.simulate_LIF_neuron(input_current=step_current, simulation_time = 100 * b2.ms)
 
-# plot I and vm
+# Plot I and vm.
 plot_tools.plot_voltage_and_current_traces(
 state_monitor, step_current, title="min input", firing_threshold=LIF.FIRING_THRESHOLD)
-print("nr of spikes: {}".format(spike_monitor.count[0]))  # should be 0
+print("nr of spikes: {}".format(spike_monitor.count[0])) # Should be 0.
+
+# Get a random parameter. Provide a random seed to have a reproducible experiment.
+random_parameters = LIF.get_random_param_set(random_seed=432)
+
+# Define your test current.
+test_current = input_factory.get_step_current(
+    t_start=..., t_end=..., unit_time=b2.ms, amplitude= ... * b2.namp)
+
+# Probe the neuron. Pass the test current AND the random params to the function.
+state_monitor, spike_monitor = LIF.simulate_random_neuron(test_current, random_parameters)
+
+# Plot.
+plot_tools.plot_voltage_and_current_traces(state_monitor, test_current, title="experiment")
+
+# Print the parameters to the console and compare with your estimates.
+# LIF.print_obfuscated_parameters(random_parameters)
+
