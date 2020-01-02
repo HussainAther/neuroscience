5e91f5c7fbdbf427606649604401e9e165121d68
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 22:22:50 -0800

    do the model

--- nd/lif.m
+++ nd/lif.m
@@ -17,3 +17,17 @@ FIRING_THRESHOLD = -50*b2.mV
 MEMBRANE_RESISTANCE = 10. * b2.Mohm
 MEMBRANE_TIME_SCALE = 8. * b2.ms
 ABSOLUTE_REFRACTORY_PERIOD = 2.0 * b2.ms
+
+# create a step current with amplitude= i_min
+step_current = input_factory.get_step_current(
+    t_start=5, t_end=100, unit_time=b2.ms,
+    amplitude= i_min)  # set i_min to your value
+
+# run the LIF model.
+# Note: As we do not specify any model parameters, the simulation runs with the default values
+(state_monitor,spike_monitor) = LIF.simulate_LIF_neuron(input_current=step_current, simulation_time = 100 * b2.ms)
+
+# plot I and vm
+plot_tools.plot_voltage_and_current_traces(
+state_monitor, step_current, title="min input", firing_threshold=LIF.FIRING_THRESHOLD)
+print("nr of spikes: {}".format(spike_monitor.count[0]))  # should be 0
