ab97a33853d53b3a288caf35019a3ff79beeb219
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 21:02:52 -0800

    init

---
+++ nd/lif.m
@@ -0,0 +1,12 @@
+import brian2 as b2
+import matplotlib.pyplot as plt
+import numpy as np
+from neurodynex.leaky_integrate_and_fire import LIF
+from neurodynex.tools import input_factory, plot_tools
+
+%{
+Leaky-integrate-and-fire (leaky integrate fire)
+}%
+
+LIF.getting_started()
+LIF.print_default_parameters()
