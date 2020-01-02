90a3cf9b6d3c2380213169126dc5620bcf28ecdf
Syed Hussain Ather <shussainather@gmail.com>
2020-01-02 11:03:01 -0800

    plot

--- nd/adex.m
+++ nd/adex.m
@@ -13,3 +13,19 @@ state_monitor, spike_monitor = AdEx.simulate_AdEx_neuron(I_stim=current, simulat
 plot_tools.plot_voltage_and_current_traces(state_monitor, current)
 print("nr of spikes: {}".format(spike_monitor.count[0]))
 # AdEx.plot_adex_state(state_monitor)
+
+# Get a random parameter. provide a random seed to have a reproducible experiment.
+random_parameters = LIF.get_random_param_set(random_seed=432)
+
+# Define your test current.
+test_current = input_factory.get_step_current(
+    t_start=..., t_end=..., unit_time=b2.ms, amplitude= ... * b2.namp)
+
+# Probe the neuron. pass the test current AND the random params to the function.
+state_monitor, spike_monitor = LIF.simulate_random_neuron(test_current, random_parameters)
+
+# Plot.
+plot_tools.plot_voltage_and_current_traces(state_monitor, test_current, title="experiment")
+
+# Print the parameters to the console and compare with your estimates.
+# LIF.print_obfuscated_parameters(random_parameters)
