3a29356241e0a516846435a1cc09e94564eef632
Syed Hussain Ather <shussainather@gmail.com>
2020-01-02 07:48:40 -0800

    plot

--- nd/eif.m
+++ nd/eif.m
@@ -26,3 +26,25 @@ V_RESET = -60.0 * b2.mV
 RHEOBASE_THRESHOLD_v_rh = -55.0 * b2.mV
 SHARPNESS_delta_T = 2.0 * b2.mV
 FIRING_THRESHOLD_v_spike = -30. * b2.mV
+
+i=1  #change i and find the value that goes into min_amp
+durations = [1,   2,    5,  10,   20,   50, 100]
+min_amp =   [0., 4.42, 0., 1.10, .70, .48, 0.]
+
+t=durations[i]
+I_amp = min_amp[i]*b2.namp
+title_txt = "I_amp={}, t={}".format(I_amp, t*b2.ms)
+
+input_current = input_factory.get_step_current(t_start=10, t_end=10+t-1, unit_time=b2.ms, amplitude=I_amp)
+
+state_monitor, spike_monitor = exp_IF.simulate_exponential_IF_neuron(I_stim=input_current, simulation_time=(t+20)*b2.ms)
+
+plot_tools.plot_voltage_and_current_traces(state_monitor, input_current,
+                                           title=title_txt, firing_threshold=exp_IF.FIRING_THRESHOLD_v_spike,
+                                          legend_location=2)
+print("nr of spikes: {}".format(spike_monitor.count[0]))
+
+plt.plot(durations, min_amp)
+plt.title("Strength-Duration curve")
+plt.xlabel("t [ms]")
+plt.ylabel("min amplitude [nAmp]")
