4184237922ef1e5f5aab36cb2f7d1af99b766916
Syed Hussain Ather <shussainather@gmail.com>
2020-01-02 17:06:49 -0800

    init var

--- nd/hopfield.m
+++ nd/hopfield.m
@@ -6,3 +6,10 @@ Hopefield network model of associative memory
 
 pattern_size = 5
 
+# Create an instance of the class HopfieldNetwork.
+hopfield_net = network.HopfieldNetwork(nr_neurons= pattern_size**2)
+# Instantiate a pattern factory.
+factory = pattern_tools.PatternFactory(pattern_size, pattern_size)
+# Create a checkerboard pattern and add it to the pattern list.
+checkerboard = factory.create_checkerboard()
+pattern_list = [checkerboard]
