8152141c26929615cf983b24338060f021d0ca21
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 08:50:09 -0800

    soma

--- NEURON/simexample.py
+++ NEURON/simexample.py
@@ -3,3 +3,13 @@ import neuron
 """
 Simulation example
 """
+
+neuron.h.load_file("stdrun.hoc")
+
+neuron.h.stdinit()
+
+soma = neuron.h.Section(name='soma')
+
+print("Soma object:", soma)
+print("Soma object name: ", soma.name())
+print("Number of segments in the soma:", soma.nseg)
