a139cc61fd0da452768e9925b04216cc55ca7a05
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 02:19:03 -0800

    plot

--- nd/oja.m
+++ nd/oja.m
@@ -1,4 +1,5 @@
 import neurodynex.ojas_rule.oja as oja
+import matplotlib.pyplot as plt
 
 """
 Oja's learning rule
@@ -9,3 +10,12 @@ cloud = oja.make_cloud()
 
 # Learn weights and return timecourse. 
 wcourse = oja.learn(cloud)  
+
+# Plot.
+plt.scatter(cloud[:, 0], cloud[:, 1], marker=".", alpha=.2)
+plt.plot(wcourse[-1, 0], wcourse[-1, 1], "or", markersize=10)
+plt.axis("equal")
+plt.figure()
+plt.plot(wcourse[:, 0], "g")
+plt.plot(wcourse[:, 1], "b")
+print("The final weight vector w is: ({},{})".format(wcourse[-1,0],wcourse[-1,1]))
