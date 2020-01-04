0a5f737e44ade0eb80dc4e4cf611309f9b5cadc3
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 08:50:51 -0800

    soma

--- NEURON/simexample.py
+++ NEURON/simexample.py
@@ -18,3 +18,12 @@ soma.L = 40
 soma.diam = 40
 print("Soma length: %f micron" % soma.L)
 print("Soma diameter: %f micron" % soma.diam)
+
+soma_area_eq = 2 * neuron.h.PI * soma.L * soma.diam / 2
+print "Soma area according to cylinder surface area equation: %f micron^2" % soma_area_eq
+
+# The 0.5 refers to the segment in the middle of the soma
+# Because there is only one segment, in this case it refers to the entire soma
+soma_area = neuron.h.area(0.5, sec=soma)
+print("Soma area according to NEURON: %f micron^2" % soma_area)
+print("Both values match: %s" % (soma_area_eq == soma_area))
