3825e8db80624a0b4d2f0fe4c157bf4e108ccd55
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 02:18:00 -0800

    learn

--- nd/oja.m
+++ nd/oja.m
@@ -3,3 +3,9 @@ import neurodynex.ojas_rule.oja as oja
 """
 Oja's learning rule
 """
+
+# Generate data points.
+cloud = oja.make_cloud()
+
+# Learn weights and return timecourse. 
+wcourse = oja.learn(cloud)  
