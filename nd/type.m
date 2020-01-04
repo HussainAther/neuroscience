2bc4db4efc1b360ba1d3ecff18966c490dbed182
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 06:46:34 -0800

    plot

--- nd/type.m
+++ nd/type.m
@@ -11,3 +11,12 @@ Type I and type II neuron models
 
 # Create an input current.
 input_current = input_factory.get_step_current(50, 150, 1.*b2.ms, 0.5*b2.pA)
+
+# Get one instance of class NeuronX and save that object in the variable 'a_neuron_of_type_X'.
+a_neuron_of_type_X = neurons.NeuronX()  # we do not know if it's type I or II
+
+# Simulate it and get the state variables.
+state_monitor = a_neuron_of_type_X.run(input_current, 200*b2.ms)
+
+# Plot state vs. time.
+neurons.plot_data(state_monitor, title="Neuron of Type X")
