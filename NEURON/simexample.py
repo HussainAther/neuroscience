8ef9e000f2b1d0f753d6717e890301aa7f1176d0
Syed Hussain Ather <shussainather@gmail.com>
2020-01-04 08:50:28 -0800

    soma

--- NEURON/simexample.py
+++ NEURON/simexample.py
@@ -13,3 +13,8 @@ soma = neuron.h.Section(name='soma')
 print("Soma object:", soma)
 print("Soma object name: ", soma.name())
 print("Number of segments in the soma:", soma.nseg)
+
+soma.L = 40
+soma.diam = 40
+print("Soma length: %f micron" % soma.L)
+print("Soma diameter: %f micron" % soma.diam)
