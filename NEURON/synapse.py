63054eac4b0ffdd51717656fe6943e9a647ed8a6
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 00:16:53 -0800

    stimulator and connection

--- NEURON/synapse.py
+++ NEURON/synapse.py
@@ -26,3 +26,11 @@ synapse = h.Exp2Syn(soma(0.5))
 synapse.tau1 = 0.5 # [ms]
 synapse.tau2 = 10.0 # [ms]
 synapse.e = -80.0 
+
+stimulator = h.VecStim()
+spike_times = [100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 950.0]
+spikes_vector = h.Vector(spike_times)
+stimulator.play(spikes_vector)
+
+connection = h.NetCon(stimulator, synapse)
+connection.weight[0] = 0.001        # [uS]
