6b84202999cebacb6a208113339e9969d2278a05
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 01:07:11 -0800

    define

--- NEURON/ansynapse.py
+++ NEURON/ansynapse.py
@@ -21,3 +21,13 @@ soma.insert("pas")
 for sec in h.allsec():
     sec.Ra = 100
     sec.cm = 1
+
+synapse = h.SimpleAMPA_NMDA(soma(0.5))
+
+stimulator = h.VecStim()
+spike_times = [100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 950.0]
+spikes_vector = h.Vector(spike_times)
+stimulator.play(spikes_vector)
+
+connection = h.NetCon(stimulator, synapse)
+connection.weight[0] = 1.0 # In units of [nS] due to the gmax scaling factor in our .mod file.
