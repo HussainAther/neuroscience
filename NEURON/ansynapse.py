9dfce9735799eb1f8712073f02310a24f843812f
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 01:33:19 -0800

    plot the rest

--- NEURON/ansynapse.py
+++ NEURON/ansynapse.py
@@ -1,5 +1,5 @@
 import neuron
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 from neuron import h
@@ -54,10 +54,26 @@ def plot_timecourse(time_array, dependent_var, newfigure=True, show=True, label=
         plt.figure()
     plt.plot(time_array, dependent_var, label=label)
     for constant in constants:
-        plt.plot(time_array, constant*numpy.ones(len(time_array)))
+        plt.plot(time_array, constant*np.ones(len(time_array)))
     plt.xlabel("Time (ms)")
     plt.ylabel(ylabel)
     if show:
         plt.show()
-        
+       
+# Plot. 
 plot_timecourse(time, v_soma)
+plot_timecourse(time, g_syn, ylabel="Conductance (uS)", label="NEURON")
+
+def dual_exp(t, tau_r, tau_d, t_start):
+    """Compute the dual exponential time course using the closed form expression."""
+    t = np.array(t)
+    time_to_peak = (tau_r*tau_d)/(tau_d-tau_r)*np.log(tau_d/tau_r)
+    factor = -np.exp(-time_to_peak/tau_r)+numpy.exp(-time_to_peak/tau_d)
+    f_dual_exp = lambda t: (np.exp(-t/tau_d) - numpy.exp(-t/tau_r))/factor
+    dual_exp = np.zeros_like(t)
+    dual_exp[t>=t_start] = f_dual_exp(t[t>=t_start]-t_start)
+    return dual_exp
+    
+plt.plot(time, 0.001*connection.weight[0]*dual_exp(time, synapse.tau_r_AMPA, synapse.tau_d_AMPA, 
+                                                   t_start=100.0+connection.delay), "r--", lw=2, label="math. expr.")
+plt.legend()
