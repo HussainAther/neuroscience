6e9b9cc88f45a5cddc252e96c0a137496fcf6c39
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 01:06:27 -0800

    soma

--- NEURON/ansynapse.py
+++ NEURON/ansynapse.py
@@ -11,3 +11,13 @@ AMPA-NMDA synapse model
 # Load external files & initialize
 neuron.h.load_file("stdrun.hoc")
 neuron.h.stdinit()
+
+soma = neuron.h.Section()
+soma.L = 40
+soma.diam = 40
+soma.insert("pas")
+
+# Configure the passive biophysics.
+for sec in h.allsec():
+    sec.Ra = 100
+    sec.cm = 1
