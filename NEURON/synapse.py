87de2bd6bfc827b3858f7670453e4e65a93e21ce
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 00:15:04 -0800

    soma

--- NEURON/synapse.py
+++ NEURON/synapse.py
@@ -11,3 +11,8 @@ Excitatory and inhibitory synapsae
 # Load external files & initialize.
 neuron.h.load_file("stdrun.hoc")
 neuron.h.stdinit()
+
+soma = neuron.h.Section()
+soma.L = 40
+soma.diam = 40
+soma.insert('pas')
