47cb8616c8d2c16cffeb8b14164aeae40c03ac76
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 03:10:27 -0800

    config

--- NEURON/synapse/stochastic.py
+++ NEURON/synapse/stochastic.py
@@ -11,3 +11,13 @@ Stochastic synapse Tsodyks-Markram Model
 # Load external files & initialize.
 neuron.h.load_file("stdrun.hoc")
 neuron.h.stdinit()
+
+soma = neuron.h.Section()
+soma.L = 40
+soma.diam = 40
+soma.insert("pas")
+
+# Configure the passive biophysics.
+for sec in h.allsec():
+    sec.Ra = 100
+    sec.cm = 1
