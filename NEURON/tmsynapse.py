85c4b5802c6af49a5302aba67f0d719b0a3bad2d
Syed Hussain Ather <shussainather@gmail.com>
2020-01-05 03:00:57 -0800

    connect

--- NEURON/tmsynapse.py
+++ NEURON/tmsynapse.py
@@ -4,10 +4,30 @@ import matplotlib.pyplot as plt
 
 from neuron import h
 
-# Load external files & initialize
+# Load external files & initialize.
 neuron.h.load_file("stdrun.hoc")
 neuron.h.stdinit()
 
 """
 Tsodyks-Markram (tsodyks markram) model.
 """
+
+soma = neuron.h.Section()
+soma.L = 40
+soma.diam = 40
+soma.insert("pas")
+
+# Configure the passive biophysics.
+for sec in h.allsec():
+    sec.Ra = 100
+    sec.cm = 1
+
+synapse = h.TsodyksMarkram_AMPA_NMDA(soma(0.5))
+
+stimulator = h.VecStim()
+spike_times = [100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 950.0]
+spikes_vector = h.Vector(spike_times)
+stimulator.play(spikes_vector)
+
+connection = h.NetCon(stimulator, synapse)
+connection.weight[0] = 1.0  # In units of [nS] due to the gmax scaling factor in our .mod file.
