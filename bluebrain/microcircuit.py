178726c98b31186bd0a49767e32f0c37c44e2a71
Syed Hussain Ather <shussainather@gmail.com>
2020-01-06 11:19:58 -0800

    download

--- bluebrain/microcircuit.py
+++ bluebrain/microcircuit.py
@@ -5,10 +5,13 @@ import neurom.viewer
 import zipfile # Extract zip files
 
 """
-The Neocortical Microcircuit Collaboration Portal (NMC Portal, 
+The Neocortical Microcircuit (microcircuit) Collaboration Portal (NMC Portal, 
 at [https://bbp.epfl.ch/nmc-portal](https://bbp.epfl.ch/nmc-portal)) 
 provides an online public resource of the Blue Brain Project's 
 first release of a digital reconstruction of the microcircuitry 
 of juvenile Rat somatosensory cortex, access to experimental 
 data sets used in the reconstruction, and the resulting single cell models.
 """
+
+# Download neocortical layer 5 thick tufted pyramidal cell model.
+urllib.urlretrieve("https://bbp.epfl.ch/nmc-portal/documents/10184/1921755/L5_TTPC2_cADpyr232_1.zip/a058fc9c-6c67-417b-a65b-20742902ccbb','L5_TTPC2_cADpyr232_1.zip")
