d39ee275b26838ebbc7cd064b43c7ba3929b7292
Syed Hussain Ather <shussainather@gmail.com>
2020-01-06 11:18:52 -0800

    init

---
+++ bluebrain/microcircuit.py
@@ -0,0 +1,14 @@
+import os
+import urllib # Download files from the web
+import neurom # Analyse / view morphologies
+import neurom.viewer
+import zipfile # Extract zip files
+
+"""
+The Neocortical Microcircuit Collaboration Portal (NMC Portal, 
+at [https://bbp.epfl.ch/nmc-portal](https://bbp.epfl.ch/nmc-portal)) 
+provides an online public resource of the Blue Brain Project's 
+first release of a digital reconstruction of the microcircuitry 
+of juvenile Rat somatosensory cortex, access to experimental 
+data sets used in the reconstruction, and the resulting single cell models.
+"""
