7b6c19f81de6b81cf605317419209cdf5638ae02
Syed Hussain Ather <shussainather@gmail.com>
2020-01-11 15:49:55 -0800

    init

---
+++ neural/receptivefield.m
@@ -0,0 +1,6 @@
+
+%{
+Model lateral geniculate nucleus (LGN) neuron that receives a target bar
+of light preceded by two masking bars of light. If positioned in space
+and time appropriately, the target bar should be less visible.
+}%
