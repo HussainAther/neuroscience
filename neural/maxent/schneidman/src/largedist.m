4cec30f71719fb92227944695363ab65f469310c
Syed Hussain Ather <shussainather@gmail.com>
2020-02-05 18:09:11 -0500

    Train model

--- neural/maxent/schneidman/src/largedist.m
+++ neural/maxent/schneidman/src/largedist.m
@@ -13,3 +13,8 @@ samples_test = spikes50(:,idx_test);
 
 % Create a pairwise maximum entropy model.
 model = maxent.createModel(50,"pairwise");
+
+% Train the model to a threshold of 1.5 standard deviations from the error of computing the marginals.
+% because the distribution is larger (50 dimensions) we cannot explicitly iterate over all 5^20 states
+% in memory and will use markov chain monte carlo (MCMC) methods to obtain an approximation.
+model = maxent.trainModel(model,samples_train,"threshold",1.5);
