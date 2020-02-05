f581dc954805fc6379ce39215851b6e636de0af0
Syed Hussain Ather <shussainather@gmail.com>
2020-02-05 18:08:14 -0500

    sample

--- neural/maxent/schneidman/src/largedist.m
+++ neural/maxent/schneidman/src/largedist.m
@@ -4,3 +4,9 @@
 % Load 50-neuron dataset.
 load data/example50
 
+% Randomly divide into train/test sets.
+[ncells,nsamples] = size(spikes50);
+idx_train = randperm(nsamples,ceil(nsamples/2));
+idx_test = setdiff(1:nsamples,idx_train);
+samples_train = spikes50(:,idx_train);
+samples_test = spikes50(:,idx_test);
