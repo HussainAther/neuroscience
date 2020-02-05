4e5582bbaadb2864bf3b24475b8b2ab01a9e1bf0
Syed Hussain Ather <shussainather@gmail.com>
2020-02-05 18:08:40 -0500

    pairwise mdoel

--- neural/maxent/schneidman/src/largedist.m
+++ neural/maxent/schneidman/src/largedist.m
@@ -10,3 +10,6 @@ idx_train = randperm(nsamples,ceil(nsamples/2));
 idx_test = setdiff(1:nsamples,idx_train);
 samples_train = spikes50(:,idx_train);
 samples_test = spikes50(:,idx_test);
+
+% Create a pairwise maximum entropy model.
+model = maxent.createModel(50,"pairwise");
