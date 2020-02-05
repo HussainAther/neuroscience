3cfb0121f838b5c51f45ae6f068c40ea02771c20
Syed Hussain Ather <shussainather@gmail.com>
2020-02-05 17:52:01 -0500

    kpairwise

--- neural/maxent/schneidman/src/smalldist.m
+++ neural/maxent/schneidman/src/smalldist.m
@@ -5,3 +5,13 @@
 % Load example spiking data of 15 neurons.
 load example15
 
+% Randomly divide it into a training set and a test set 
+% (so we can verify how well we trained).
+[ncells,nsamples] = size(spikes15);
+idx_train = randperm(nsamples,ceil(nsamples/2));
+idx_test = setdiff(1:nsamples,idx_train);
+samples_train = spikes15(:,idx_train);
+samples_test = spikes15(:,idx_test);
+
+% Create a k-pairwise model (pairwise maxent with synchrony constraints).
+model = maxent.createModel(ncells,"kising");
