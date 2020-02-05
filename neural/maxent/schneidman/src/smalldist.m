886b4cc66f7c26b7732bf0e8eff37e87e22117b2
Syed Hussain Ather <shussainather@gmail.com>
2020-02-05 17:50:45 -0500

    load example

--- neural/maxent/schneidman/src/smalldist.m
+++ neural/maxent/schneidman/src/smalldist.m
@@ -1,3 +1,7 @@
 % Find the marginals (firing rates and correlations) of
 % sample data, and compare them against the maximum entropy
 % model predictions.
+
+% Load example spiking data of 15 neurons.
+load example15
+
