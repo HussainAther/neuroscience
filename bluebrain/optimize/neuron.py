d25c4596d02b45feda19fe4da780df6e233a9927
Syed Hussain Ather <shussainather@gmail.com>
2020-02-13 15:47:39 -0500

    read inf iles

--- bluebrain/optimize/neuron.py
+++ bluebrain/optimize/neuron.py
@@ -4,3 +4,16 @@ import matplotlib.pyplot as plt
 """
 Optimize parameters for a simple neuron model.
 """
+
+fig, ax = plt.subplots(2, sharey = True)
+
+for i, filename in enumerate({"dep_trace.txt", "hyp_trace.txt"}):
+    response = urllib2.urlopen("https://bbp.epfl.ch/public/MOOC_HBP/{}".format(filename))
+    data = response.read()
+    with open(filename, "w") as f:
+        f.write(data)
+        
+    trace = numpy.loadtxt(filename)    
+    ax[i].plot(trace[:,0], trace[:,1], color = "k")
+    ax[i].set_ylabel("Voltage (mV)")
+    ax[i].set_xlabel("Time (ms)")
