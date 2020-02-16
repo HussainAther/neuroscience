551fc6ab1c801a292e7ab8c4ecaed5046eb0baf6
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 21:59:47 -0500

    init var

--- channel/hh.py
+++ channel/hh.py
@@ -6,3 +6,12 @@ from scipy.integrate import odeint
 """
 Hodgkin-Huxley (Hodgkin Huxley) using scipy
 """
+
+# Constants
+C_m = 1.0  # uF/cm^2
+g_Na = 120.0  # mS/cm^2
+g_K = 36.0
+g_L = 0.3
+V_Na = 50.0  # mV
+V_K = -77.0
+V_L = -54.402
