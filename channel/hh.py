ecbbfa0373c0fd88ddc4adf464f5eca2d6570a13
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 22:00:47 -0500

    define each membrane value

--- channel/hh.py
+++ channel/hh.py
@@ -15,3 +15,11 @@ g_L = 0.3
 V_Na = 50.0  # mV
 V_K = -77.0
 V_L = -54.402
+
+def alpha_m(V): return 0.1 * (V + 40.0) / (1.0 - np.exp(-0.1 * (V + 40.0)))
+def beta_m(V):  return 4.0 * np.exp(-0.0556 * (V + 65.0))
+def alpha_h(V): return 0.07 * np.exp(-0.05 * (V + 65.0))
+def beta_h(V):  return 1.0 / (1.0 + np.exp(-0.1 * (V + 35.0)))
+def alpha_n(V): return 0.01 * (V + 55.0) / (1.0 - np.exp(-0.1 * (V + 55.0)))
+def beta_n(V):  return 0.125 * np.exp(-0.0125 * (V + 65))
+
