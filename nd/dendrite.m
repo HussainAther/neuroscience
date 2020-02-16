99a74c9772d671a7d9c792319087bdee23d535cb
Syed Hussain Ather <shussainather@gmail.com>
2020-02-16 16:09:07 -0500

    comment fix

--- nd/dendrite.m
+++ nd/dendrite.m
@@ -9,14 +9,14 @@ Dendrites and passive cable equation
 %}
 
 passive_cable.getting_started()
-CABLE_LENGTH = 500. * b2.um  # length of dendrite
-CABLE_DIAMETER = 2. * b2.um  # diameter of dendrite
-R_LONGITUDINAL = 0.5 * b2.kohm * b2.mm  # Intracellular medium resistance
-R_TRANSVERSAL = 1.25 * b2.Mohm * b2.mm ** 2  # cell membrane resistance (->leak current)
-E_LEAK = -70. * b2.mV  # reversal potential of the leak current (-> resting potential)
-CAPACITANCE = 0.8 * b2.uF / b2.cm ** 2  # membrane capacitance
+CABLE_LENGTH = 500. * b2.um  % length of dendrite
+CABLE_DIAMETER = 2. * b2.um  % diameter of dendrite
+R_LONGITUDINAL = 0.5 * b2.kohm * b2.mm  % Intracellular medium resistance
+R_TRANSVERSAL = 1.25 * b2.Mohm * b2.mm ** 2  % cell membrane resistance (->leak current)
+E_LEAK = -70. * b2.mV  % reversal potential of the leak current (-> resting potential)
+CAPACITANCE = 0.8 * b2.uF / b2.cm ** 2  % membrane capacitance
 
-# spatial indexing
+% spatial indexing
 voltage_monitor, cable_model = passive_cable.simulate_passive_cable(...)
 probe_location = 0.123 * b2.mm
 v = voltage_monitor[cable_model.morphology[probe_location]].v
