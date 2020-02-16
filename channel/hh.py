601eefa2608feaa3b6b033235bd9ba02d607c0d1
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 22:02:17 -0500

    current val's

--- channel/hh.py
+++ channel/hh.py
@@ -23,3 +23,6 @@ def beta_h(V):  return 1.0 / (1.0 + np.exp(-0.1 * (V + 35.0)))
 def alpha_n(V): return 0.01 * (V + 55.0) / (1.0 - np.exp(-0.1 * (V + 55.0)))
 def beta_n(V):  return 0.125 * np.exp(-0.0125 * (V + 65))
 
+def I_Na(V,m,h): return g_Na * m**3 * h * (V - V_Na)
+def I_K(V, n): return g_K * n**4 * (V - V_K)
+def I_L(V): return g_L * (V - V_L)
