003f30e3cc7e2a8d6f80ab5dd3ad0bf0c19d631a
Syed Hussain Ather <shussainather@gmail.com>
2020-02-16 16:09:21 -0500

    comment fix

--- nd/type.m
+++ nd/type.m
@@ -9,19 +9,19 @@ from neurodynex.neuron_type import neurons
 Type I and type II neuron models
 %}
 
-# Create an input current.
+% Create an input current.
 input_current = input_factory.get_step_current(50, 150, 1.*b2.ms, 0.5*b2.pA)
 
-# Get one instance of class NeuronX and save that object in the variable 'a_neuron_of_type_X'.
-a_neuron_of_type_X = neurons.NeuronX()  # we do not know if it's type I or II
+% Get one instance of class NeuronX and save that object in the variable 'a_neuron_of_type_X'.
+a_neuron_of_type_X = neurons.NeuronX()  % we do not know if it's type I or II
 
-# Simulate it and get the state variables.
+% Simulate it and get the state variables.
 state_monitor = a_neuron_of_type_X.run(input_current, 200*b2.ms)
 
-# Plot state vs. time.
+% Plot state vs. time.
 neurons.plot_data(state_monitor, title="Neuron of Type X")
 
-# Get an instance of class NeuronY.
-a_neuron_of_type_Y = neurons.NeuronY()  # we do not know if it's type I or II
+% Get an instance of class NeuronY.
+a_neuron_of_type_Y = neurons.NeuronY()  % we do not know if it's type I or II
 state_monitor = a_neuron_of_type_Y.run(input_current, 200*b2.ms)
 neurons.plot_data(state_monitor, title="Neuron of Type Y")
