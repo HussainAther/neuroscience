c920f62db074ac1337563cdba9eb9f347dfe0ec9
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 18:14:31 -0500

    begin class

--- spike/izhikevich.py
+++ spike/izhikevich.py
@@ -1,3 +1,16 @@
+import numpy as np
+
 """
 Izhikevich model for firing
 """
+
+class IzhNeuron:
+    def __init__(self, label, a, b, c, d, v0, u0=None):
+        self.label = label
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+       
+        self.v = v0
+        self.u = u0 if u0 is not None else b*v0
