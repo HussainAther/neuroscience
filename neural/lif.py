54588e96c4e599d3d87dbc7d80206692eaadf824
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 17:56:10 -0500

    init var

--- neural/lif.py
+++ neural/lif.py
@@ -4,3 +4,9 @@ import numpy as np
 """
 Leaky integrate and fire (leaky-integrate-and-fire LIF) 
 """
+
+# Initialize vairables.
+T = 50 # total time to simulate (msec)
+dt = 0.125 # simulation time step (msec)
+time = arange(0, T+dt, dt) # time array
+trest = 0 # initial refractory time
