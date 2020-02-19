be5f544a011ee61108549832ae7a689a898c3b1a
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 14:23:21 -0500

    model it

--- neural/theta.py
+++ neural/theta.py
@@ -7,3 +7,15 @@ and later used in various fields of computational neuroscience. The model
 is particularly well suited to describe neuron bursting, which are rapid 
 oscillations in the membrane potential of a neuron interrupted by periods of relatively little oscillation.
 """
+
+def I(t):
+    """
+    Adjust current input over time t.
+    """
+    return 0 
+
+def dthetadt(theta, t):
+    """
+    dtheta/dt = 1 - cos(theta) + (1 + cos(theta)) I(t)
+    """
+    return 1 - np.cos(theta) + (1 + np.cos(theta)) * I(t)
