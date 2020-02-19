aa9d3aeab16755440d0241ca567c1128690bed1d
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 18:15:22 -0500

    intio intio classo

--- spike/izhikevich.py
+++ spike/izhikevich.py
@@ -5,12 +5,15 @@ Izhikevich model for firing
 """
 
 class IzhNeuron:
+    """
+    Initialize a class for variables of the neuron
+    for differential equation use.
+    """
     def __init__(self, label, a, b, c, d, v0, u0=None):
         self.label = label
         self.a = a
         self.b = b
         self.c = c
         self.d = d
-       
         self.v = v0
         self.u = u0 if u0 is not None else b*v0
