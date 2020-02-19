feb5f46328e45471766d9ad2be78c4e079d3f636
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 14:18:26 -0500

    init

---
+++ neural/theta.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""
+The theta model, or Ermentrout–Kopell canonical model, is a biological 
+neuron model originally developed to model neurons in the animal Aplysia, 
+and later used in various fields of computational neuroscience. The model 
+is particularly well suited to describe neuron bursting, which are rapid 
+oscillations in the membrane potential of a neuron interrupted by periods of relatively little oscillation.
+"""
