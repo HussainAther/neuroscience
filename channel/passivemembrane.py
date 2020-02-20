8c047b10ce058634e42fe5e90050fc069de99636
Syed Hussain Ather <shussainather@gmail.com>
2020-02-19 22:06:12 -0500

    vars

--- channel/passivemembrane.py
+++ channel/passivemembrane.py
@@ -10,3 +10,8 @@ def bepswI(dt, Tfin):
     """
     Bakcwards Euler passive membrane 
     """
+    VCl = -68 # mV source voltage
+    A = 4*np.pi*1e-6 # cm^2 patch area
+    Cm = 1 # micro F/cm^2 capacitance per area
+    gCl = .3 # mS/cm^2 conductance
+    tau = Cm/gCl # ms time constant	
