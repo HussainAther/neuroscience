80da8692c02741b211c4f563b5a60f26164040aa
Syed Hussain Ather <shussainather@gmail.com>
2020-02-20 17:48:53 -0500

    init

---
+++ info/coherence.m
@@ -0,0 +1,4 @@
+%{
+The autocovariance of Y is C_YY = (2alpha^2/pi) * arcsin(C_XX/sigma_X^2+l^2)
+Use this result and Exercise 7 to derive numerically the coherence between X and Y when X is white noise with a cut-off frequency of 100 Hz. Show that this allows you to reproduce the red curve in Figure 18.1D. 
+%}
