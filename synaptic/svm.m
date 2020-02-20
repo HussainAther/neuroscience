d0b10c8a53d7428b73ef35e5433592d3fcb4b0a2
Syed Hussain Ather <shussainather@gmail.com>
2020-02-20 15:07:23 -0500

    cmment fix

--- synaptic/svm.m
+++ synaptic/svm.m
@@ -1,6 +1,6 @@
 %{
 MATLAB's existing SVM (support vector machine) model.
-}%
+%}
 t = 200;
 data = squeeze(cat(3,l_eeg(:,t,:),r_eeg(:,t,:)))'; trueLabels = [ones(size(l_eeg,3),1); ...
               2*ones(size(r_eeg,3),1)];
