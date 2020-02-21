4b6685bea8401216b816e9a2f653bee71f7cc2b9
Syed Hussain Ather <shussainather@gmail.com>
2020-02-21 18:19:11 -0500

    init

---
+++ info/gammafit.m
@@ -0,0 +1,4 @@
+%{Simulation of an integrate-and-fire neuron with random, gamma distributed 
+threshold of order 10 stimulated with a sinusoidal current.
+Plot hte prediction instantaneous rate of a corresponding unmodulated process.
+}%
