05f667d4a9e8abb8c0b00d00104144f7bd25b7d4
Syed Hussain Ather <shussainather@gmail.com>
2020-03-16 16:35:27 -0400

    dubquotes

--- info/signaltonoise.py
+++ info/signaltonoise.py
@@ -1,15 +1,15 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import nitime.utils as utils
 import nitime.timeseries as ts
 import nitime.viz as viz
+import numpy as np
 
-'''
+"""
 Calculate signal-to-noise (signal to noise snr) ratio. Where SNR(omega) is the ratio of the signal power and the noise power at the 
 frequency band centered on \omega.This equation holde true for a Gaussian channel and is an upper bound for all other cases.
 The signal power is estimated as the power of the mean response to repeated presentations of the same signal and the noise power 
 is calculated as the average of the power in the deviation from this average in each trial
-'''
+"""
 
 # Generate auto-regressive sequence as the signal
 ar_seq, nz, alpha = utils.ar_generator(N=128, drop_transients=10)
@@ -22,7 +22,7 @@ sample = []
 fig_tseries = []
 
 # Add noise to ar_seq to demonstrate effects of adding noise on signal to noise ratio and the 
-# calculated information
+# calculated information.
 for idx, noise in enumerate([1, 10, 50, 100]):
     sample.append(np.ones((n_trials, ar_seq.shape[-1])) + ar_seq)
     n_points = sample[-1].shape[-1]
@@ -33,16 +33,16 @@ sample_mean = np.mean(sample[-1], 0)
 fig_tseries.append(plt.figure())
 ax = fig_tseries[-1].add_subplot(1, 1, 1)
 ax.plot(sample[-1].T)
-ax.plot(ar_seq, 'b', linewidth=4)
-ax.plot(sample_mean, 'r', linewidth=4)
-ax.set_xlabel('Time')
-ax.set_ylabel('Amplitude')
+ax.plot(ar_seq, "b", linewidth=4)
+ax.plot(sample_mean, "r", linewidth=4)
+ax.set_xlabel("Time")
+ax.set_ylabel("Amplitude")
 
 # low noise
 tseries = ts.TimeSeries(sample[-1], sampling_rate=1.)
 fig_snr.append(viz.plot_snr(tseries))
 
-# plot to compare information transmission and the signal to noise ratio between last two noise levels
+# Plot to compare information transmission and the signal to noise ratio between last two noise levels.
 ts1 = ts.TimeSeries(sample[-1], sampling_rate=1.)
 ts2 = ts.TimeSeries(sample[-2], sampling_rate=1.)
 fig_compare = viz.plot_snr_diff(ts1, ts2)
--- maxent/genSample.py
+++ maxent/genSample.py
@@ -1,12 +1,12 @@
 
-'''
+"""
 Sample from a maximum entropy model.
-'''
+"""
 
 def generateSamples(model, nsamples, burnin=1000):
-    '''
+    """
     Accept a model and number of samples to be generated via Metropolis-Hastings (M-H MH).
     Drop the first 1000 generated samples (burn in) and skip most of the samples generated along the 
     way. 
-    '''
+    """
     
