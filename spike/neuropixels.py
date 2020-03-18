c8b0171022e7e29123673d9e4fb999986790f05b
Syed Hussain Ather <shussainather@gmail.com>
2020-03-17 23:34:26 -0400

    diff load methods

--- spike/neuropixels.py
+++ spike/neuropixels.py
@@ -6,3 +6,23 @@ import pandas as pd
 """
 Neuropixels dataset from the Allen Institute
 """
+
+# Load data.
+import platform
+platstring = platform.platform()
+
+if "Darwin" in platstring:
+    # OS X 
+    data_root = "/Volumes/Brain2019/"
+elif "Windows"  in platstring:
+    # Windows (replace with the drive letter of USB drive)
+    data_root = "E:/"
+elif ("amzn1" in platstring):
+    # then on AWS
+    data_root = "/data/"
+else:
+    # then your own linux platform
+    # EDIT location where you mounted hard drive
+    data_root = "/media/$USERNAME/Brain2019/"
+
+manifest_path = os.path.join(data_root, "dynamic-brain-workshop/visual_coding_neuropixels/2019/manifest.json")
