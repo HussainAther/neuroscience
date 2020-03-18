23364b5270769f783867270c5a5b1d126603628d
Syed Hussain Ather <shussainather@gmail.com>
2020-03-17 20:44:42 -0400

    init var

--- connectivity/connectomics.py
+++ connectivity/connectomics.py
@@ -8,3 +8,11 @@ from analysisdatalink.datalink_ext import AnalysisDataLinkExt as AnalysisDataLin
 """
 Functionality for electron microscopy dataset
 """
+
+dataset_name = "pinky100"
+data_version = 175
+sqlalchemy_database_uri = "postgres://postgres:welcometothematrix@swdb-em-db.crjvviai1xxh.us-west-2.rds.amazonaws.com"
+dl = AnalysisDataLink(dataset_name=dataset_name,
+                      sqlalchemy_database_uri=sqlalchemy_database_uri,
+                      materialization_version=data_version,
+                      verbose=False)
