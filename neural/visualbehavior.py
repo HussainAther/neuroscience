66ba435dcc69daf91e736056374e4f20e07085f2
Syed Hussain Ather <shussainather@gmail.com>
2020-03-18 15:04:06 -0400

    set style

--- neural/visualbehavior.py
+++ neural/visualbehavior.py
@@ -4,6 +4,10 @@ import os
 import pandas as pd
 import seaborn as sns
 
+sns.set_context("notebook", font_scale=1.5, rc={"lines.markeredgewidth": 2})
+sns.set_style("white")
+sns.set_palette("deep")
+
 """
 Visual behavior
 """
