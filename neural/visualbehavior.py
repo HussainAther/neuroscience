3d4ee65f4644ddba372143f2ac813c05dc992a56
Syed Hussain Ather <shussainather@gmail.com>
2020-03-18 15:07:49 -0400

    import allen

--- neural/visualbehavior.py
+++ neural/visualbehavior.py
@@ -1,8 +1,11 @@
+import allensdk.brain_observatory.behavior.swdb.utilities as tools
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import seaborn as sns
+
+from allensdk.brain_observatory.behavior.swdb import behavior_project_cache as bpc
 
 sns.set_context("notebook", font_scale=1.5, rc={"lines.markeredgewidth": 2})
 sns.set_style("white")
