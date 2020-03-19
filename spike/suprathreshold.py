c49813373b827566b78952691ab18c74deb7a0fa
Syed Hussain Ather <shussainather@gmail.com>
2020-03-19 18:11:02 -0400

    more comment and return

--- spike/suprathreshold.py
+++ spike/suprathreshold.py
@@ -9,5 +9,7 @@ def Vt(I, R, phi, tau_m):
     """
     Current term V(t) depends on mean input current and reacts to changes
     in presynaptic spike rate on the time scale of hte synaptic time constant
-    tau_syn.
+    tau_syn. We use an ensemble avergae of the current values I, spike threshold phi,
+    membrane time constant tau_m, and resistance R.
     """
+    return (np.average(I)*R-phi)/tau_m
