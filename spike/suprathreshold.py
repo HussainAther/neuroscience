ef027407c3678f75b21927e56e1d1be2ccf6f1dd
Syed Hussain Ather <shussainather@gmail.com>
2020-03-19 18:08:59 -0400

    def

--- spike/suprathreshold.py
+++ spike/suprathreshold.py
@@ -3,3 +3,11 @@ import numpy as np
 """
 Membrane potential increases monotonically in the suprathreshold regime.
 """
+
+# Given a positive derivative, the current term in the rate equation is
+def Vt(I, R, phi, tau_m):
+    """
+    Current term V(t) depends on mean input current and reacts to changes
+    in presynaptic spike rate on the time scale of hte synaptic time constant
+    tau_syn.
+    """
