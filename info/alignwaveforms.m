cdbb1ac19805a11446c44d4204737130a7f3787d
Syed Hussain Ather <shussainather@gmail.com>
2020-03-22 17:43:32 -0400

    input output

--- info/alignwaveforms.m
+++ info/alignwaveforms.m
@@ -2,3 +2,6 @@ function out = align_waveforms(x)
     % Determine alignments for a set of waves
     % relative to the initial waveform using
     % cross correlation.
+    % Takes x as an MxN matrix of waveforms where x(m,:) is the 
+    % nth waveform and outputs a vector of length M where out(m) is
+    % the offset relative to the first wave.
