f28eb6986ab6f77a253780c72e0e45d59f401347
Syed Hussain Ather <shussainather@gmail.com>
2020-03-22 19:04:41 -0400

    events

--- info/plotpsth.m
+++ info/plotpsth.m
@@ -5,4 +5,8 @@ function psth,bins= bin_for_psth(raw_dta, sampling_rate, threshold, trial_count,
     % threshold, number of contiguous trials trial_count, length of each trial trial_length,
     % and size of each PSTH bin bin_size, output the psth (count in each bin) and bins 
     % (center position of ech bin, in seconds relative to trial start). All time units 
-    % are in seconds
+    % are in seconds.
+    events = raw_data > threshold;
+    % only positive threshold crossings (not sustained activity above threshold)
+    events = diff(events) == 1;
+    events = [0 events];
