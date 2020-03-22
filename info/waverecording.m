9225ba57630b9b761247f57dbb0caa012bfaaadd
Syed Hussain Ather <shussainather@gmail.com>
2020-03-22 18:46:24 -0400

    end date

--- info/waverecording.m
+++ info/waverecording.m
@@ -22,13 +22,13 @@ classdef pcm_recording < recording
             return obj.sample_rate;
         end
         function data = raw_data(obj)
-            fid = fopen(obj.filename, "r");
+            fid = fopen(obj.filename, 'r');
             if fid == -1
-                error("Unable to open file" + obj.filename);
+                error('Unable to open file' + obj.filename);
                 data = [];
                 return
             else
-                data = fread(fid, inf, "unit16 = > double", 0, "l");
+                data = fread(fid, inf, 'unit16 = > double', 0, 'l');
                 fclose(fid);
             end
         end
@@ -44,3 +44,9 @@ function dates = threshold_crossings(wav_filename, start_time)
     threshold_times = find(threshold_crossings);
     % threshold_times is the sample count since start_time
     % for each threshold crossing
+    threshold_sec = threshold_times / sampling_rate;
+    dates = zeros(length(threshold_sec), 1));
+    for ii = 1:length(threshold_sec)
+        dates[ii] = addtodate(datenum(start_time, threshold_sec, 'second'));
+    end
+end
