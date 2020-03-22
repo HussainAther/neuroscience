72630f6e9c48cfb576c08fb0c650b2ca51e7f909
Syed Hussain Ather <shussainather@gmail.com>
2020-03-22 19:02:52 -0400

    comment

--- info/plotpsth.m
+++ info/plotpsth.m
@@ -1,3 +1,8 @@
 function psth,bins= bin_for_psth(raw_dta, sampling_rate, threshold, trial_count, trial_length, bin_size)
     % Locate evens above threshold in raw data and generate PSTH (peristimulus time histogram)
     % from multi-trial recording. Trials should be contiguous.
+    % For input data raw_data, sampling rate in Hz, sampling_rtae, threshold for events
+    % threshold, number of contiguous trials trial_count, length of each trial trial_length,
+    % and size of each PSTH bin bin_size, output the psth (count in each bin) and bins 
+    % (center position of ech bin, in seconds relative to trial start). All time units 
+    % are in seconds
