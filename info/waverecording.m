d868e01d3384a4becb369dbabf26819b193d96e9
Syed Hussain Ather <shussainather@gmail.com>
2020-03-22 18:09:04 -0400

    end all

--- info/waverecording.m
+++ info/waverecording.m
@@ -3,4 +3,11 @@ classdef wav_recording < recording
         function obj = wav_recording(filename)
             obj = obj@recording(filename);
         end
-      
+        function r = sample_rate(obj)
+            [data, r] = wavread(obj.filename);
+        end
+        function data = raw_data(obj)
+            [data, r] = wavread(obj.filename);
+        end
+    end
+end 
