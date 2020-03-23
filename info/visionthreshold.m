6a87d80d4bb51cfb1845b064b408e3d226c8f725
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:08:51 -0400

    brightness test point

--- info/visionthreshold.m
+++ info/visionthreshold.m
@@ -1,3 +1,7 @@
 % Stimulus program for psychophysics experiment
 temp = unit8(zeros(500, 500, 3)); % Create a dark stimulus matrix.
 temp1 = cell(10, 1); % Create a cell that can hold 10 matrices.
+for ii = 1:10 
+    temp(200, 200, :) = 255; % Insert a fixation point.
+    temp(200, 240, :) = (ii-1)*10; % Insert a test point 40 pixels right of it
+                                   % with brightness range 0 to 90.
