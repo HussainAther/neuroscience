89179324fc8f961e0cf5eaf2d3b4db6bc53237df
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:11:20 -0400

    modified mtrix

--- info/visionthreshold.m
+++ info/visionthreshold.m
@@ -5,3 +5,4 @@ for ii = 1:10
     temp(200, 200, :) = 255; % Insert a fixation point.
     temp(200, 240, :) = (ii-1)*10; % Insert a test point 40 pixels right of it
                                    % with brightness range 0 to 90.
+    temp1(ii) = temp; % Put the respective modified matrix in cell.
