34cf6b4d81af0e525957cc09c80ee42d6ef4f9ac
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:18:08 -0400

    coefs

--- info/cwt.m
+++ info/cwt.m
@@ -1,2 +1,6 @@
 % Continuous wavelet transform (cwt) 
-
+function coefs 5 simple_cwt(t, x, mother_wavelet, max_wavelet, scales, params)
+    % Generates coefs for a continuous wavelet transform
+    % t, x are time and data points for time series data
+    % mother_wavelet is a function, taking parameters (t, params),
+    % where the value of params depends on the specific function used
