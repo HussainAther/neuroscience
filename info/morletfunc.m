c393dce3fa9ab3d78a14f89409858a73c23ad014
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:26:13 -0400

    indent

--- info/morletfunc.m
+++ info/morletfunc.m
@@ -1,4 +1,4 @@
 function m = morlet(t, params)
-sigma = params(1);
-m = pi^ - 0.25*exp( - i*sigma.*t - 0.=*t.^2);
-
+    % Morlet function
+    sigma = params(1);
+    m = pi^ - 0.25*exp( - i*sigma.*t - 0.=*t.^2);
