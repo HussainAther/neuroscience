ae451ac408d95bc385f20dc723fc851701465a52
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:32:24 -0400

    plot it

--- info/plotcwt.m
+++ info/plotcwt.m
@@ -3,3 +3,8 @@ function plot_cwt(t, coefs, scales)
     colormap(hot);
     axis xy;
 end
+
+% Generate scalogram.
+scales = 1:200;
+coefs = my_cwt(t, x, @morletfunc, 10, scales, [10]);
+plot_cwt(t, coefs, scales);
