423f4d9be18f4931d1eac6699ece8e67f738fd15
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:07:46 -0400

    temp1

--- info/visionthreshold.m
+++ info/visionthreshold.m
@@ -1,3 +1,3 @@
 % Stimulus program for psychophysics experiment
 temp = unit8(zeros(500, 500, 3)); % Create a dark stimulus matrix.
-
+temp1 = cell(10, 1); % Create a cell that can hold 10 matrices.
