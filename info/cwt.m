67c7b6e42d183c5d32fb6351997b250b90b05fec
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:18:28 -0400

    params

--- info/cwt.m
+++ info/cwt.m
@@ -4,3 +4,7 @@ function coefs 5 simple_cwt(t, x, mother_wavelet, max_wavelet, scales, params)
     % t, x are time and data points for time series data
     % mother_wavelet is a function, taking parameters (t, params),
     % where the value of params depends on the specific function used
+    % max_wavelet is the maximum range of the wavelet function (beyond which
+    % the wavelet is essentially zero)
+    % scales is a vector of desired scales
+    % params is the parameter for the mother wavelet function
