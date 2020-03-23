5085ffe5c3a344d96189f91494d992f91d86a7c2
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:12:25 -0400

    randperm

--- info/visionthreshold.m
+++ info/visionthreshold.m
@@ -6,3 +6,8 @@ for ii = 1:10
     temp(200, 240, :) = (ii-1)*10; % Insert a test point 40 pixels right of it
                                    % with brightness range 0 to 90.
     temp1(ii) = temp; % Put the respective modified matrix in cell.
+end
+h = figure; % Create a figure.
+stimulusorder = randperm(200); % Create a random order from 1 to 200
+                               % for the 200 trials. This lets you have 
+                               % a precisely equal number per condition.
