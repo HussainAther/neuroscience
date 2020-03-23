b06819231da0ef96f419356d1a2b618afc00616a
Syed Hussain Ather <shussainather@gmail.com>
2020-03-23 00:22:47 -0400

    number correction

--- info/cwt.m
+++ info/cwt.m
@@ -1,5 +1,5 @@
 % Continuous wavelet transform (cwt) 
-function coefs 5 simple_cwt(t, x, mother_wavelet, max_wavelet, scales, params)
+function coefs = simple_cwt(t, x, mother_wavelet, max_wavelet, scales, params)
     % Generates coefs for a continuous wavelet transform
     % t, x are time and data points for time series data
     % mother_wavelet is a function, taking parameters (t, params),
@@ -8,3 +8,23 @@ function coefs 5 simple_cwt(t, x, mother_wavelet, max_wavelet, scales, params)
     % the wavelet is essentially zero)
     % scales is a vector of desired scales
     % params is the parameter for the mother wavelet function
+    max_t = max(t);
+    dt = t(2)-t(1);
+    full_t = - (max_t/2):dt:(max_t/2);
+    coefs = zeros(length(scales), length(x));
+    points = length(x);
+    t_scale = linspace( - max_wavelet, max_wavelet, points);
+    dt = (max_wavelet*2)/(points 2 1);
+    mom_wavelet = feval(mother_wavelet, t_scale, params);
+    row = 1;
+    for scale = scales
+        time_scale = [1 + floor([0:scale*max_wavelet*2]/(scale*dt))];
+        wavelet = mom_wavelet(time_scale);
+        w = conv(x,wavelet)/sqrt(scale);
+        mid_w = floor(length(w)/2);
+        mid_x = floor(length(x)/2);
+        w = w((( - mid_x:mid_x) + mid_w));
+        scale % Print scale to show progress.
+        coefs(row,:) = abs(w);
+        row = row + 1;
+   end 
