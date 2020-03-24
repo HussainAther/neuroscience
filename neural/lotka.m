a2f5087d70771b2cf1c0a56f0237bbb2971e2ec0
Syed Hussain Ather <shussainather@gmail.com>
2020-03-24 17:59:09 -0400

    init

---
+++ neural/lotka.m
@@ -0,0 +1,7 @@
+function lotka
+    % Lotka-Volterra (lotka volterra) ODEs for showing dynamics of the systems
+    % Y1 --[k1]--> 2*Y1            Reproduction of preys
+    % Y1 + Y2 --[k2]--> 2*Y2       Prey-Predator interaction
+    % Y2 --[k3]-->                 Predators death
+    %
+    % Y1 is number of prey species, Y2 is  number of predator species
