42b79534f402ff8dbb30bf54fffcd4f950812f73
Syed Hussain Ather <shussainather@gmail.com>
2020-03-27 01:32:47 -0400

    quote

--- neural/isochrons.m
+++ neural/isochrons.m
@@ -7,7 +7,7 @@ function isochrons(F,phases,x0)
     m = 200; % spatial grid
     k = 5; % the number of skipped cycles
     [t,lc] = ode23s(F,0:tau:T,x0); % forward integration
-    dx = (max(lc)-min(lc))’/m; % spatial resolution
+    dx = (max(lc)-min(lc))'/m; % spatial resolution
     center = (max(lc)+min(lc))’/2; % center of the limit cycle
     iso = [x0-m^0.5*dx, x0+m^0.5*dx]; % isochron’s initial segment
     [t,lc] = ode23s(F,0:tau:T,x0); % forward integration
