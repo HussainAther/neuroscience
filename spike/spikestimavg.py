1cb1e474d3e5bb2b18c52ac3e1fdbdc20adbbc25
Syed Hussain Ather <shussainather@gmail.com>
2020-04-28 16:51:15 -0400

    print intervals

--- spike/spikestimavg.py
+++ spike/spikestimavg.py
@@ -4,3 +4,7 @@ spikes = mat["rho"]
 stimVals = mat["stim"]
 n_timeSteps = 150
 spikeTimes = spikes[n_timeSteps:].nonzero()[0] + n_timeSteps
+sepInterval = np.array([2,20,40,60,80,100])
+sepInterval = (sepInterval/2).astype("int")
+print("Time intervals:", sepInterval, "steps")
+
