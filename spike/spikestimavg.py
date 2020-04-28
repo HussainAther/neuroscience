8df381c47509e39233c349a25882d447baf13daf
Syed Hussain Ather <shussainather@gmail.com>
2020-04-28 16:51:40 -0400

    colros and handles

--- spike/spikestimavg.py
+++ spike/spikestimavg.py
@@ -7,4 +7,8 @@ spikeTimes = spikes[n_timeSteps:].nonzero()[0] + n_timeSteps
 sepInterval = np.array([2,20,40,60,80,100])
 sepInterval = (sepInterval/2).astype("int")
 print("Time intervals:", sepInterval, "steps")
-
+colors = ["b", "g", "r", "c", "m", "y"]
+handles = [Rectangle((0,0),1,1,color=c,ec="k") for c in colors]
+labels = [str(int(x*2)) + " ms" for x in sepInterval]
+MAX_SINGLE_RESPONSE = 29.472907029165032
+diff_dict = dict()
